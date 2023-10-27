b3ec230f00ea42d4f7f2488f9ad09848d427f26b
Maarten Flippo <maartenflippo@outlook.com>
2023-10-27 13:44:12 +0200

    Cleanup int_times propagator:
    * Add more tests
    * Cleanup memory of explanations

--- pumpkin-lib/src/propagators/int_times.rs
+++ pumpkin-lib/src/propagators/int_times.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use log::warn;
+
 use crate::{
     basic_types::{variables::IntVar, Predicate, PropagationStatusCP, PropositionalConjunction},
     engine::{
@@ -16,7 +18,9 @@ pub struct IntTimes<VA, VB, VC> {
     b: PropagatorVariable<VB>,
     c: PropagatorVariable<VC>,
 
-    propagations: [HashMap<DomainChange, [Predicate; 2]>; 3],
+    propagations_a: HashMap<DomainChange, [Predicate; 2]>,
+    propagations_b: HashMap<DomainChange, [Predicate; 2]>,
+    propagations_c: HashMap<DomainChange, [Predicate; 2]>,
 }
 
 pub struct IntTimesArgs<VA, VB, VC> {
@@ -46,7 +50,9 @@ where
             b: context.register(args.b, DomainEvent::Any, ID_B),
             c: context.register(args.c, DomainEvent::Any, ID_C),
 
-            propagations: Default::default(),
+            propagations_a: Default::default(),
+            propagations_b: Default::default(),
+            propagations_c: Default::default(),
         })
     }
 }
@@ -80,7 +86,7 @@ where
         let new_min_c = a_min * b_min;
 
         if context.upper_bound(&self.c) > new_max_c {
-            self.propagations[2].insert(
+            self.propagations_c.insert(
                 DomainChange::UpperBound(new_max_c),
                 [predicate![self.a <= a_max], predicate![self.b <= b_max]],
             );
@@ -88,7 +94,7 @@ where
         }
 
         if context.lower_bound(&self.c) < new_min_c {
-            self.propagations[2].insert(
+            self.propagations_c.insert(
                 DomainChange::LowerBound(new_min_c),
                 [predicate![self.a >= a_min], predicate![self.b >= b_min]],
             );
@@ -99,7 +105,7 @@ where
         if b_max >= 1 {
             let bound = (c_min + b_max - 1) / b_max;
             if context.lower_bound(&self.a) < bound {
-                self.propagations[0].insert(
+                self.propagations_a.insert(
                     DomainChange::LowerBound(bound),
                     [predicate![self.c >= c_min], predicate![self.b <= b_max]],
                 );
@@ -111,7 +117,7 @@ where
         if b_min >= 1 {
             let bound = c_max / b_min;
             if context.upper_bound(&self.a) > bound {
-                self.propagations[0].insert(
+                self.propagations_a.insert(
                     DomainChange::UpperBound(bound),
                     [predicate![self.c <= c_max], predicate![self.b >= b_min]],
                 );
@@ -124,7 +130,7 @@ where
             let bound = (c_min + a_max - 1) / a_max;
 
             if context.lower_bound(&self.b) < bound {
-                self.propagations[1].insert(
+                self.propagations_b.insert(
                     DomainChange::LowerBound(bound),
                     [predicate![self.c >= c_min], predicate![self.a <= a_max]],
                 );
@@ -136,7 +142,7 @@ where
         if a_min >= 1 {
             let bound = c_max / a_min;
             if context.upper_bound(&self.b) > bound {
-                self.propagations[1].insert(
+                self.propagations_b.insert(
                     DomainChange::UpperBound(bound),
                     [predicate![self.c <= c_max], predicate![self.a >= a_min]],
                 );
@@ -147,7 +153,23 @@ where
         Ok(())
     }
 
-    fn synchronise(&mut self, _context: &PropagationContext) {}
+    fn synchronise(&mut self, context: &PropagationContext) {
+        fn cleanup<Var: IntVar>(
+            context: &PropagationContext,
+            var: &PropagatorVariable<Var>,
+            propagations: &mut HashMap<DomainChange, [Predicate; 2]>,
+        ) {
+            propagations.retain(|change, _reason| match change {
+                DomainChange::Removal(value) => !context.contains(var, *value),
+                DomainChange::LowerBound(bound) => context.lower_bound(var) >= *bound,
+                DomainChange::UpperBound(bound) => context.upper_bound(var) <= *bound,
+            });
+        }
+
+        cleanup(context, &self.a, &mut self.propagations_a);
+        cleanup(context, &self.b, &mut self.propagations_b);
+        cleanup(context, &self.c, &mut self.propagations_c);
+    }
 
     fn get_reason_for_propagation(
         &mut self,
@@ -155,9 +177,9 @@ where
         delta: Delta,
     ) -> PropositionalConjunction {
         let reason = match delta.affected_local_id() {
-            ID_A => self.propagations[0][&self.a.unpack(delta)],
-            ID_B => self.propagations[1][&self.b.unpack(delta)],
-            ID_C => self.propagations[2][&self.c.unpack(delta)],
+            ID_A => self.propagations_a[&self.a.unpack(delta)],
+            ID_B => self.propagations_b[&self.b.unpack(delta)],
+            ID_C => self.propagations_c[&self.c.unpack(delta)],
             _ => unreachable!(),
         };
 
@@ -173,6 +195,11 @@ where
     }
 
     fn initialise_at_root(&mut self, context: &mut PropagationContext) -> PropagationStatusCP {
+        if context.lower_bound(&self.a).is_negative() || context.lower_bound(&self.b).is_negative()
+        {
+            warn!("IntTimes does not support variables with negative bounds, but it is instantiated with them. The behavior will likely not be correct.");
+        }
+
         self.propagate(context)
     }
 
@@ -227,7 +254,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::engine::test_helper::TestSolver;
+    use crate::{conjunction, engine::test_helper::TestSolver};
 
     use super::*;
 
@@ -248,5 +275,79 @@ mod tests {
         assert_eq!(4, solver.upper_bound(b));
         assert_eq!(0, solver.lower_bound(c));
         assert_eq!(12, solver.upper_bound(c));
+
+        let reason_lb = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_C, DomainChange::LowerBound(0)),
+        );
+        assert_eq!(conjunction!([a >= 1] & [b >= 0]), reason_lb);
+
+        let reason_ub = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_C, DomainChange::UpperBound(12)),
+        );
+        assert_eq!(conjunction!([a <= 3] & [b <= 4]), reason_ub);
+    }
+
+    #[test]
+    fn bounds_of_a_and_c_propagate_bounds_b() {
+        let mut solver = TestSolver::default();
+        let a = solver.new_variable(2, 3);
+        let b = solver.new_variable(0, 12);
+        let c = solver.new_variable(2, 12);
+
+        let mut propagator = solver.new_propagator::<IntTimes<_, _, _>>(IntTimesArgs { a, b, c });
+
+        solver.propagate(&mut propagator).expect("no empty domains");
+
+        assert_eq!(2, solver.lower_bound(a));
+        assert_eq!(3, solver.upper_bound(a));
+        assert_eq!(1, solver.lower_bound(b));
+        assert_eq!(6, solver.upper_bound(b));
+        assert_eq!(2, solver.lower_bound(c));
+        assert_eq!(12, solver.upper_bound(c));
+
+        let reason_lb = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_B, DomainChange::LowerBound(1)),
+        );
+        assert_eq!(conjunction!([a <= 3] & [c >= 2]), reason_lb);
+
+        let reason_ub = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_B, DomainChange::UpperBound(6)),
+        );
+        assert_eq!(conjunction!([a >= 2] & [c <= 12]), reason_ub);
+    }
+
+    #[test]
+    fn bounds_of_b_and_c_propagate_bounds_a() {
+        let mut solver = TestSolver::default();
+        let a = solver.new_variable(0, 10);
+        let b = solver.new_variable(3, 6);
+        let c = solver.new_variable(2, 12);
+
+        let mut propagator = solver.new_propagator::<IntTimes<_, _, _>>(IntTimesArgs { a, b, c });
+
+        solver.propagate(&mut propagator).expect("no empty domains");
+
+        assert_eq!(1, solver.lower_bound(a));
+        assert_eq!(4, solver.upper_bound(a));
+        assert_eq!(3, solver.lower_bound(b));
+        assert_eq!(6, solver.upper_bound(b));
+        assert_eq!(2, solver.lower_bound(c));
+        assert_eq!(12, solver.upper_bound(c));
+
+        let reason_lb = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_A, DomainChange::LowerBound(1)),
+        );
+        assert_eq!(conjunction!([b <= 6] & [c >= 2]), reason_lb);
+
+        let reason_ub = solver.get_reason(
+            &mut propagator,
+            Delta::new(ID_A, DomainChange::UpperBound(4)),
+        );
+        assert_eq!(conjunction!([b >= 3] & [c <= 12]), reason_ub);
     }
 }
