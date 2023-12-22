f4614af5d0e07ae7728a3ea6e535b8d11edec2c5
imarijnissen <i.c.w.m.marijnissen@student.tudelft.nl>
2023-12-22 09:41:51 +0100

    Recalculating upon synchronisation lazily rather than eagerly

--- pumpkin-lib/src/propagators/cumulative/time_table/time_table_per_point_incremental.rs
+++ pumpkin-lib/src/propagators/cumulative/time_table/time_table_per_point_incremental.rs
@@ -21,14 +21,23 @@ use super::time_table_propagator::{should_enqueue, ResourceProfile, TimeTablePro
 
 /// Propagator responsible for using time-table reasoning to propagate the [Cumulative] constraint - This method creates a [ResourceProfile] per time point rather than creating one over an interval
 pub struct TimeTablePerPointIncrementalProp<Var> {
-    /// * `time_table` - Structure responsible for holding the time-table; currently it consists of a map to avoid unnecessary allocation for time-points at which no [ResourceProfile] is present | Assumptions: The time-table is sorted based on start time and none of the profiles overlap - generally, it is assumed that the calculated [ResourceProfile]s are maximal
+    /// Structure responsible for holding the time-table; currently it consists of a map to avoid unnecessary allocation for time-points at which no [ResourceProfile] is present |
+    /// Assumptions: The time-table is sorted based on start time and none of the profiles overlap - generally, it is assumed that the calculated [ResourceProfile]s are maximal
     time_table: BTreeMap<u32, ResourceProfile<Var>>,
-    /// * `reasons_for_propagation_lower_bound` - For each variable, eagerly maps the explanation of the lower-bound change
+    /// For each variable, eagerly maps the explanation of the lower-bound change;
+    /// For a task i (representing a map in the [Vec]), \[bound\] stores the explanation for \[s_i >= bound\]
     reasons_for_propagation_lower_bound: Vec<HashMap<i32, PropositionalConjunction>>,
-    /// * `reasons_for_propagation_upper_bound` - For each variable, eagerly maps the explanation of the upper-bound change
+    /// For each variable, eagerly maps the explanation of the upper-bound change
+    /// For a task i (representing a map in the [Vec]), \[bound\] stores the explanation for \[s_i <= bound\]
     reasons_for_propagation_upper_bound: Vec<HashMap<i32, PropositionalConjunction>>,
-    /// * `cumulative_params` - Stores the input parameters to the cumulative constraint
-    cumulative_params: CumulativeParameters<Var>,
+    /// Stores the input parameters to the cumulative constraint
+    parameters: CumulativeParameters<Var>,
+    /// Keeps track of whether the current state of the time-table is outdated (i.e. whether it needs to be recalculated from scratch)
+    /// 
+    /// Imagine the situation where a synchronisation takes place and the propagator eagerly recalculates the time-table
+    /// but then another propagator finds a conflict and the time-table calculation was superfluous;
+    /// this flag ensures that the recalculation is done lazily, only when required
+    time_table_outdated: bool,
 }
 
 impl<Var> CPPropagatorConstructor for CumulativeArgs<Var, TimeTablePerPointIncrementalProp<Var>>
@@ -48,14 +57,15 @@ where
 }
 
 impl<Var: IntVar + 'static> TimeTablePerPointIncrementalProp<Var> {
-    pub fn new(params: CumulativeParameters<Var>) -> TimeTablePerPointIncrementalProp<Var> {
+    pub fn new(parameters: CumulativeParameters<Var>) -> TimeTablePerPointIncrementalProp<Var> {
         let reasons_for_propagation: Vec<HashMap<i32, PropositionalConjunction>> =
-            vec![HashMap::new(); params.tasks.len()];
+            vec![HashMap::new(); parameters.tasks.len()];
         TimeTablePerPointIncrementalProp {
             time_table: BTreeMap::new(),
             reasons_for_propagation_lower_bound: reasons_for_propagation.to_vec(),
             reasons_for_propagation_upper_bound: reasons_for_propagation,
-            cumulative_params: params,
+            parameters,
+            time_table_outdated: false,
         }
     }
 }
@@ -67,54 +77,61 @@ impl<Var: IntVar + 'static> ConstraintProgrammingPropagator
         pumpkin_assert_advanced!(
             Util::check_bounds_equal_at_propagation(
                 context,
-                &self.cumulative_params.tasks,
-                &self.cumulative_params.bounds,
+                &self.parameters.tasks,
+                &self.parameters.bounds,
             ),
             "Bound were not equal when propagating"
         );
-        for Updated {
-            task,
-            old_lower_bound,
-            old_upper_bound,
-            new_lower_bound,
-            new_upper_bound,
-        } in self.cumulative_params.updated.iter()
-        {
-            //Go over all of the updated tasks and calculate the added mandatory part (we know that for each of these tasks, a mandatory part exists, otherwise it would not have been added (see [TimeTablePropagator::should_propagate]))
-            let (lower_update_range, upper_update_range) = generate_update_range(
+        if self.time_table_outdated {
+            // The time-table needs to be recalculated from scratch anyways so we perform the calculation now
+            TimeTablePropagator::reset_structures(self, context);
+            self.time_table_outdated = false;
+        } else {
+            for Updated {
                 task,
-                *old_lower_bound,
-                *old_upper_bound,
-                *new_lower_bound,
-                *new_upper_bound,
-            );
-
-            for t in lower_update_range
-                .into_iter()
-                .chain(upper_update_range.into_iter())
+                old_lower_bound,
+                old_upper_bound,
+                new_lower_bound,
+                new_upper_bound,
+            } in self.parameters.updated.iter()
             {
-                //We first go over the lower update range and then we go over the lower update range
-                pumpkin_assert_extreme!(
-                    !self.time_table.contains_key(&(t as u32))
-                    || !self.time_table.get(&(t as u32)).unwrap().profile_tasks.iter().any(|profile_task| profile_task.id.unpack() as usize == task.id.unpack() as usize),
-                    "Attempted to insert mandatory part where it already exists at time point {t} for task {} in time-table per time-point propagator", task.id.unpack() as usize);
+                //Go over all of the updated tasks and calculate the added mandatory part (we know that for each of these tasks, a mandatory part exists, otherwise it would not have been added (see [TimeTablePropagator::should_propagate]))
+                let (lower_update_range, upper_update_range) = generate_update_range(
+                    task,
+                    *old_lower_bound,
+                    *old_upper_bound,
+                    *new_lower_bound,
+                    *new_upper_bound,
+                );
 
-                //Add the updated profile to the ResourceProfile at time t
-                let current_profile: &mut ResourceProfile<Var> = self
-                    .time_table
-                    .entry(t as u32)
-                    .or_insert(ResourceProfile::default(t));
-                current_profile.height += task.resource_usage;
-                current_profile.profile_tasks.push(Rc::clone(task));
-                if current_profile.height > self.cumulative_params.capacity {
-                    //The newly introduced mandatory part(s) caused an overflow of the resource
-                    return Util::create_error_clause(context, &current_profile.profile_tasks);
+                for t in lower_update_range
+                    .into_iter()
+                    .chain(upper_update_range.into_iter())
+                {
+                    //We first go over the lower update range and then we go over the lower update range
+                    pumpkin_assert_extreme!(
+                        !self.time_table.contains_key(&(t as u32))
+                        || !self.time_table.get(&(t as u32)).unwrap().profile_tasks.iter().any(|profile_task| profile_task.id.unpack() as usize == task.id.unpack() as usize),
+                        "Attempted to insert mandatory part where it already exists at time point {t} for task {} in time-table per time-point propagator", task.id.unpack() as usize);
+
+                    //Add the updated profile to the ResourceProfile at time t
+                    let current_profile: &mut ResourceProfile<Var> = self
+                        .time_table
+                        .entry(t as u32)
+                        .or_insert(ResourceProfile::default(t));
+                    current_profile.height += task.resource_usage;
+                    current_profile.profile_tasks.push(Rc::clone(task));
+                    if current_profile.height > self.parameters.capacity {
+                        //The newly introduced mandatory part(s) caused an overflow of the resource
+                        return Util::create_error_clause(context, &current_profile.profile_tasks);
+                    }
                 }
             }
         }
-        self.cumulative_params.updated.clear(); //We have processed all of the updates, we can clear the structure
-                                                //We pass the entirety of the table to check due to the fact that the propagation of the current profile could lead to the propagation across multiple profiles
-                                                //For example, if we have updated 1 resource profile which caused a propagation then this could cause another propagation by a profile which has not been updated
+
+        self.parameters.updated.clear(); //We have processed all of the updates, we can clear the structure
+                                         //We pass the entirety of the table to check due to the fact that the propagation of the current profile could lead to the propagation across multiple profiles
+                                         //For example, if we have updated 1 resource profile which caused a propagation then this could cause another propagation by a profile which has not been updated
         let CumulativePropagationResult {
             status,
             explanations,
@@ -131,11 +148,11 @@ impl<Var: IntVar + 'static> ConstraintProgrammingPropagator
     fn synchronise(&mut self, context: &PropagationContext) {
         Util::reset_bounds_clear_updated(
             context,
-            &mut self.cumulative_params.updated,
-            &mut self.cumulative_params.bounds,
-            &self.cumulative_params.tasks,
+            &mut self.parameters.updated,
+            &mut self.parameters.bounds,
+            &self.parameters.tasks,
         );
-        TimeTablePropagator::reset_structures(self, context);
+        self.time_table_outdated = true;
     }
 
     fn get_reason_for_propagation(
@@ -147,7 +164,7 @@ impl<Var: IntVar + 'static> ConstraintProgrammingPropagator
             delta,
             &self.reasons_for_propagation_lower_bound,
             &self.reasons_for_propagation_upper_bound,
-            &self.cumulative_params.tasks,
+            &self.parameters.tasks,
         )
     }
 
@@ -157,9 +174,9 @@ impl<Var: IntVar + 'static> ConstraintProgrammingPropagator
         local_id: crate::engine::LocalId,
         _event: crate::engine::OpaqueDomainEvent,
     ) -> EnqueueDecision {
-        let updated_task = Rc::clone(&self.cumulative_params.tasks[local_id.unpack() as usize]);
+        let updated_task = Rc::clone(&self.parameters.tasks[local_id.unpack() as usize]);
         should_enqueue(
-            &mut self.cumulative_params,
+            &mut self.parameters,
             updated_task,
             context,
             self.time_table.is_empty(),
@@ -175,7 +192,7 @@ impl<Var: IntVar + 'static> ConstraintProgrammingPropagator
     }
 
     fn initialise_at_root(&mut self, context: &mut PropagationContext) -> PropagationStatusCP {
-        Util::initialise_at_root(true, &mut self.cumulative_params, context);
+        Util::initialise_at_root(true, &mut self.parameters, context);
         let CumulativePropagationResult {
             status,
             explanations,
@@ -222,11 +239,11 @@ impl<Var: IntVar + 'static> TimeTablePropagator<Var> for TimeTablePerPointIncrem
 
     fn create_time_table(
         context: &PropagationContext,
-        params: &CumulativeParameters<Var>,
+        parameters: &CumulativeParameters<Var>,
     ) -> Result<Self::TimeTableType, Vec<Rc<Task<Var>>>> {
         let mut profile: BTreeMap<u32, ResourceProfile<Var>> = BTreeMap::new();
         //First we go over all tasks and determine their mandatory parts
-        for task in params.tasks.iter() {
+        for task in parameters.tasks.iter() {
             let upper_bound = context.upper_bound(&task.start_variable);
             let lower_bound = context.lower_bound(&task.start_variable);
 
@@ -240,7 +257,7 @@ impl<Var: IntVar + 'static> TimeTablePropagator<Var> for TimeTablePerPointIncrem
                     current_profile.height += task.resource_usage;
                     current_profile.profile_tasks.push(Rc::clone(task));
 
-                    if current_profile.height > params.capacity {
+                    if current_profile.height > parameters.capacity {
                         //The addition of the current task to the resource profile has caused an overflow
                         return Err(current_profile.profile_tasks.clone());
                     }
@@ -251,7 +268,7 @@ impl<Var: IntVar + 'static> TimeTablePropagator<Var> for TimeTablePerPointIncrem
     }
 
     fn get_parameters(&self) -> &CumulativeParameters<Var> {
-        &self.cumulative_params
+        &self.parameters
     }
 
     fn get_time_table(&self) -> Vec<&ResourceProfile<Var>> {
