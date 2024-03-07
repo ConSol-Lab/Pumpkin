914d08f454c48aa0eec94cade314445263b5b5a6
Jeff Smits <j.smits-1@tudelft.nl>
2024-03-07 14:36:16 +0000

    Merged in feature/faster-ci (pull request #124)
    Feature/faster ci
    
    * Make pre-commit faster and invalidate CI caches properly
    * Restrict cargo-deny to a specific version to avoid building/installing updates all the time during CI/pre-commit
    * Update deny.toml to version 2
    * Improve pipeline/pre-commit scripts
    
    
    Approved-by: Emir Demirović

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -2,5 +2,12 @@
 
 set -eux
 
-rustup toolchain install --no-self-update --profile minimal --component rustfmt -- nightly
+if [ "$1" = "--pre-commit" ]; then
+    # We don't install/update nightly in pre-commit unless there isn't one. Update it yourself
+    if ! rustup toolchain list | grep --silent 'nightly-[a-z]'; then
+        rustup toolchain install --no-self-update --profile minimal --component rustfmt -- nightly
+    fi
+else
+    rustup toolchain install --no-self-update --profile minimal --component rustfmt -- nightly
+fi
 cargo +nightly fmt --check 
