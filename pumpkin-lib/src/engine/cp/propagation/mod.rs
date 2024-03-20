071c3bdaf406eda32f4865f9015b73d3e92f8862
Emir Demirović <emir.demirovic@gmail.com>
2024-03-20 16:09:01 +0000

    Merged in refactor/propagator_docs_and_usage (pull request #145)
    The idea is to streamline the process of propagator development, especially for newcommers. Changes:
    
    Added more documentation on how to implement a propagator, that serves as a nice checklist/guideline for propagator development. See the mod file in propagation.
    
    Added default implementations to the Propagator trait to make it easier to get the first working propagator version, and some supporting documentation.
    
    Moved propagator documentation from the propagator folder to the propagation folder. The propagator folder now holds only concrete implementations, whereas propagation explains the entire concept and provides guidelines on how to implement propagators.
    
    A side effect is that debug_propagate_from_scratch now requires providing explanations for its propagations, because it is used in the default implementation of propagate.
    
    Approved-by: Maarten Flippo

--- pumpkin-lib/src/engine/cp/propagation/mod.rs
+++ pumpkin-lib/src/engine/cp/propagation/mod.rs
@@ -1,5 +1,27 @@
 //! Contains the main building blocks for propagators.
 //!
+//! # Theoretical
+//!
+//! A propagator takes as input a set of variables (`x_i ∈ X`) and for each variable a corresponding
+//! domain (`D_i ∈ D`); it can then be seen as a function which maps `D ↦ D'` such that `D'_i ⊆ D_i`
+//! for all variables (i.e. the domain of a variable either remains the same after applying the
+//! propagator or it becomes a subset of the domain before applying the propagator).
+//!
+//! An example of a propagator can be the simple not equal (`!=`) propagator, suppose that
+//! we have two variables `x ∈ {0}` and `y ∈ {0, 1}` and the constraint `x != y`. The not equal
+//! propagator will then take as input the variables `x` and `y` and their respective domains
+//! `D = {D_x = {0}, D_y = {0, 1}` and produce a new domain `D' = {D'_x = {0}, D'_y = {1}}` for
+//! which we can see that `D_x = D'_x` and `D'_y ⊆ D_y`.
+//!
+//! A propagator is said to be at fix-point if `D_x = D'_x` meaning that no further propagations
+//! can take place when applying the propagator. A propagator is said to be "idempotent" if a single
+//! call to it will result in it being at fix-point.
+//!
+//! For more information about the construction of these types of propagation-based solvers, we
+//! refer to [\[1\]](https://dl.acm.org/doi/pdf/10.1145/1452044.1452046).
+//!
+//! # Practical
+//!
 //! Each concrete propagator is associated with two structs, which implement traits with
 //! corresponding names:
 //! - [`Propagator`]: contains most of the propagator logic.
@@ -15,20 +37,48 @@
 //! [`Propagator::notify`]). The idea behind using the structs apart
 //! from Propagator is to support 'views' on variables.
 //!
+//! We do not require *any* propagator to be idempotent (see the previous section for a
+//! definition) and it can be assumed that if a propagator is not at fix-point after propagating
+//! that it will be called again by the solver until no further propagations happen.
+//!
 //! See the [`propagators`] folder for concrete propagator implementations.
 //!
 //! # How to implement a new propagator?
-//! The typical workflow for implementing a new propagator comes down to implementing the following:
-//! - A propagator struct that implements the [`Propagator`] trait. Within the propagator, domain
-//!   changes on variables are done using [`PropagatorVariable`]s.
-//! - A constructor struct that implements [`PropagatorConstructor`], which contains necessary
-//!   information to create the propagator. For example, it may contain an array of IntegerVariables
-//!   and some constants that will be used by the propagator.
-//! - The [`PropagatorConstructor`] contains the logic needed to create the propagator. Note that
-//!   [`PropagatorConstructor`] receives [`PropagatorConstructorContext`], which is used to retrieve
-//!   [`PropagatorVariable`]s.
+//!
+//! We recommend the following workflow:
+//! - Implement a propagator struct that implements the [`Propagator`] trait. For now only implement
+//!   the required functions, i.e., [`Propagator::debug_propagate_from_scratch`] and
+//!   [`Propagator::name`].
+//!     * Note that [`PropagatorVariable`]s are to be used as the variables within the propagator,
+//!       so all domain changes go through [`PropagatorVariable`]s.
+//! - Implement a struct that implements the [`PropagatorConstructor`] trait, which contains
+//!   necessary information to create the propagator.
+//!     * For example, it may contain an array of IntegerVariables and some constants that will be
+//!       used by the propagator.
+//!     * Note that [`PropagatorConstructor`] receives [`PropagatorConstructorContext`], which is
+//!       used to retrieve [`PropagatorVariable`]s.
+//! - Following the procedure above gives a simple propagator version that is likely not efficient,
+//!   but has an important role for testing. Now is a good time to write tests which use the a test
+//!   solver. This will greatly help with testing the propagator.
+//!     * For example, see the tests in the
+//!       [`crate::propagators::arithmetic::absolute_value::absolute_value.rs`].
+//! - The next step towards a more efficient propagator is to implement [`Propagator::notify`] and
+//!   [`Propagator::notify_literal`]. Depending on the concrete propagator, this may only make sense
+//!   when done in together with the next step.
+//! - Afterwards step is to implement the remaining functions, i.e., [`Propagator::propagate`],
+//!   [`Propagator::synchronise`], and [`Propagator::initialise_at_root`]. These are all
+//!   interdependent.
+//! - Decide on the priortiy of the propagator, i.e., implement [`Propagator::priority`].
+//! - Make sure to write new tests and run all tests throughout the process.
+//! - The propagator implementation is now done!
+//!
 //! The propagator is added to the solver through [`ConstraintSatisfactionSolver::add_propagator`]
 //! by passing the [`PropagatorConstructor`] as an argument.
+//!
+//! //! # Bibliography
+//!
+//! \[1\] C. Schulte and P. J. Stuckey, ‘Efficient constraint propagation engines’, ACM Transactions
+//! on Programming Languages and Systems (TOPLAS), vol. 31, no. 1, pp. 1–43, 2008.
 
 pub(crate) mod local_id;
 pub(crate) mod propagation_context;
