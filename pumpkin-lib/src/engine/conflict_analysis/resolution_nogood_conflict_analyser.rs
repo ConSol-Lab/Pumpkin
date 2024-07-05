b6cc712c2253820c236900553f82fd2f91215263
EmirD <emir.demirovic@gmail.com>
2024-07-05 09:38:22 +0200

    pseudo-code for conflict analysis

--- pumpkin-lib/src/engine/conflict_analysis/resolution_nogood_conflict_analyser.rs
+++ pumpkin-lib/src/engine/conflict_analysis/resolution_nogood_conflict_analyser.rs
@@ -15,6 +15,14 @@ impl ResolutionNogoodConflictAnalyser {
     // The asserting nogood is at position [0], and the second decision level positioned predicate
     // is at position [1].
     pub fn compute_1uip(&mut self, _context: &mut ConflictAnalysisNogoodContext) -> LearnedNogood {
+        // get conflict nogood
+        // while not propagating
+        //      take the predicate last assigned on the trail
+        //      get its reason
+        //          for now no lazy, so just ask for the reason based on the trail entry
+        //      bump the LBD and activity of the reason
+        //      replace it with its reason
+        //      do semantic minimisation -> not strictly necessary?
         todo!();
     }
 }
