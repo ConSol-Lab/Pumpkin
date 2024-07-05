b693bf38791c17e73c658c2d06bf62b44eb27d96
EmirD <emir.demirovic@gmail.com>
2024-07-05 12:47:30 +0200

    minor comments

--- pumpkin-lib/src/engine/conflict_analysis/resolution_nogood_conflict_analyser.rs
+++ pumpkin-lib/src/engine/conflict_analysis/resolution_nogood_conflict_analyser.rs
@@ -13,13 +13,18 @@ pub struct LearnedNogood {
 }
 
 impl ResolutionNogoodConflictAnalyser {
+    // General comment:
+    // Much of the code below is implemented with simplicity in mind rather than efficiency.
+    // If this turns out to work fine, then we could look into addressing the todos.
+    // It could also very well be the case that this inefficiency is very minor.
+
     // Computes the nogood according to the 1UIP scheme.
     // The asserting nogood is at position [0], and the second decision level positioned predicate
     // is at position [1].
     pub fn compute_1uip(&mut self, context: &mut ConflictAnalysisNogoodContext) -> LearnedNogood {
-        // temporary functions that will eventually be removed
-        // using these to not change the structure of the solver too much in the early stages of
-        // this implementation
+        // temporary functions that will eventually be removed.
+        // I am using these to not change the structure of the solver too much in the early stages
+        // of this implementation, but hopefully in the near future this would not be needed.
         let int_pred = |predicate: &Predicate| {
             match predicate {
             Predicate::IntegerPredicate(integer_predicate) => *integer_predicate,
@@ -29,6 +34,7 @@ impl ResolutionNogoodConflictAnalyser {
         }
         };
 
+        // todo: could be done more efficiently with additional data structures.
         let is_nogood_propagating = |nogood: &Vec<Predicate>| {
             let num_predicates_at_current_decision_level = nogood
                 .iter()
@@ -53,6 +59,7 @@ impl ResolutionNogoodConflictAnalyser {
             .average_conflict_size
             .add_term(learned_nogood.len() as u64);
 
+        // Keep refining the nogood until it propagates!
         while !is_nogood_propagating(&learned_nogood) {
             // take the predicate last assigned on the trail
             let next_predicate = int_pred(
@@ -66,19 +73,17 @@ impl ResolutionNogoodConflictAnalyser {
                     })
                     .expect("Cannot have an empty nogood during analysis."),
             );
-            // todo: we are not doing lazy,
-            // so just ask for the reason based on the trail entry
-            let reason = context.get_propagation_reason(&next_predicate);
-            // todo: bump the LBD and activity of the reason
-            // replace the next_predicate with its reason
+            // Replace the next_predicate with its reason. This is done in two steps:
+            // 1) Remove the predicate from the nogood.
             let next_predicate_index = learned_nogood
                 .iter()
                 .position(|predicate| int_pred(predicate) == next_predicate)
                 .expect("Must be able to find the predicate again.");
             let _ = learned_nogood.swap_remove(next_predicate_index);
-            // replace the next_predicate by its reason
+            // 2) Add the reason of the next_predicate to the nogood.
+            let reason = context.get_propagation_reason(&next_predicate);
             for predicate in reason {
-                // todo: do semantic minimisation - not strictly necessary
+                // todo: do semantic minimisation - not strictly necessary for the first version
                 if !learned_nogood.contains(&predicate) {
                     learned_nogood.push(predicate);
                 }
