26fbb2539b57b29e3083a7d9874c35a332d8970d
EmirD <emir.demirovic@gmail.com>
2024-07-08 07:58:20 +0200

    temporary removed the more refined implementation of the nogood propagator, going to a simpler one before to set up other parts

--- pumpkin-lib/src/propagators/nogood/nogood_propagator.rs
+++ pumpkin-lib/src/propagators/nogood/nogood_propagator.rs
@@ -3,6 +3,7 @@ use std::ops::Not;
 use crate::basic_types::ConstraintOperationError;
 use crate::basic_types::PropagationStatusCP;
 use crate::basic_types::PropositionalConjunction;
+use crate::basic_types::StorageKey;
 use crate::engine::predicates::integer_predicate::IntegerPredicate;
 use crate::engine::predicates::predicate::Predicate;
 use crate::engine::propagation::PropagationContextMut;
@@ -11,9 +12,24 @@ use crate::engine::propagation::PropagatorConstructor;
 use crate::engine::propagation::PropagatorConstructorContext;
 use crate::engine::AssignmentsInteger;
 
-// todo: at the time of creation, the variables get subscribe to the propagator, but later creating
-// new variables does not subscribe them automatically!
-// todo: hardcode the notifications.
+// TODO:
+// Define data structures.
+// Notify should accumulate things to propagate.
+// Propagating goes through the data structures?
+//
+// todo: for now we do not accumulate the updates, we simply update as we enqueued them.
+// todo: could specialise the propagator for binary integers. This is a general comment.
+
+// Need also LBD, and nogood clean ups! Remove root level true predicates.
+
+// Todo: add predicate compression for 1) logging nogoods, and 2) smaller memory footprint.
+
+// Current data structure:
+// [domain_id][operation] -> [list of A], where A is list of watchers, where a watcher where is:
+
+// Say for operation >=
+// A is a list of pairs (bound, nogood)
+
 #[derive(Debug, Clone, Copy)]
 pub struct NogoodPropagatorConstructor {}
 
@@ -21,21 +37,60 @@ impl PropagatorConstructor for NogoodPropagatorConstructor {
     type Propagator = NogoodPropagator;
 
     fn create(self, mut _context: PropagatorConstructorContext<'_>) -> Self::Propagator {
-        NogoodPropagator { nogoods: vec![] }
+        NogoodPropagator::default()
     }
 }
 
+// #[derive(Default, Clone, Debug)]
+// struct Watcher {
+// This field represents the right-hand side of the predicate present in the nogood.
+// It is used as an indicator to whether the nogood should be inspected.
+// right_hand_side: i32,
+// nogood_id: NogoodId,
+// todo: consider the cached literal
+// }
+//
+// #[derive(Default, Clone, Debug)]
+// struct WatchList {
+// nogoods with a watched predicate [x >= k]
+// lower_bound: Vec<Watcher>,
+// nogoods with a watched predicate [x <= k]
+// upper_bound: Vec<Watcher>,
+// nogoods with a watched predicate [x != k]
+// hole: Vec<Watcher>,
+// nogoods with a watched predicate [x == k]
+// assignment: Vec<Watcher>,
+// }
+//
+// #[derive(Clone, Debug)]
+// enum Update {
+// LowerBound {
+// old_lower_bound: i32,
+// new_lower_bound: i32,
+// },
+// UpperBound {
+// old_upper_bound: i32,
+// new_upper_bound: i32,
+// },
+// Hole {
+// hole_value: i32,
+// },
+// Assignment {
+// assigned_value: i32,
+// },
+// }
+
 #[derive(Default, Clone, Debug)]
 pub struct NogoodPropagator {
+    #[allow(dead_code)]
     nogoods: Vec<Vec<IntegerPredicate>>,
+    // nogoods: KeyedVec<NogoodId, Vec<IntegerPredicate>>,
+    // Watch lists for the nogood propagator.
+    // todo: could improve the data structure for watching.
+    // watch_lists: HashMap<DomainId, WatchList>,
+    // enqueued_updates: Vec<Update>,
 }
 
-// Probably the nogood propagator is just a propagator with additional functionality.
-// But somehow all variables are subscribed to it? Actually subscribing goes dynamically!
-// Or it is simply treated in a special way...I suppose the nogood propagator is a special case
-// 'global' propagator that is potentially tied to all variables? Actually it only cares about
-// specific predicates, but this is probably best if it internally tracks this.
-// Need to ensure that it is always enqueued!
 impl NogoodPropagator {
     #[allow(dead_code)]
     fn add_permanent_nogood(
@@ -52,7 +107,8 @@ impl NogoodPropagator {
         todo!();
     }
 
-    fn propagate_nogood(
+    #[allow(dead_code)]
+    fn debug_propagate_nogood_from_scratch(
         &self,
         nogood: &[IntegerPredicate],
         context: &mut PropagationContextMut,
@@ -111,12 +167,46 @@ impl NogoodPropagator {
         }
         Ok(())
     }
+
+    // fn add_watcher(&mut self, predicate: IntegerPredicate, nogood_id: NogoodId) {
+    // todo: look up Hashset operations to do this properly.
+    // Create an entry in case the predicate is missing
+    // if !self.watch_lists.contains_key(&predicate.get_domain()) {
+    // let _ = self
+    // .watch_lists
+    // .insert(predicate.get_domain(), WatchList::default());
+    // }
+    //
+    // match predicate {
+    // IntegerPredicate::LowerBound {
+    // domain_id,
+    // lower_bound,
+    // } => todo!(),
+    // IntegerPredicate::UpperBound {
+    // domain_id,
+    // upper_bound,
+    // } => todo!(),
+    // IntegerPredicate::NotEqual {
+    // domain_id,
+    // not_equal_constant,
+    // } => todo!(),
+    // IntegerPredicate::Equal {
+    // domain_id,
+    // equality_constant,
+    // } => todo!(),
+    // }
+    //
+    //         self.watch_lists
+    // .get_mut(&predicate.get_domain())
+    // .unwrap()
+    // .push(Watcher { nogood_id });
+    // }
 }
 
 impl Propagator for NogoodPropagator {
     fn name(&self) -> &str {
-        // In the current hacky version, it is important to keep this name exactly this.
-        // The solver looks for a propagator with this name and adds the learned nogood.
+        // It is important to keep this name exactly this.
+        // In parts of code for debugging, it looks for this particular name.
         "NogoodPropagator"
     }
 
@@ -124,32 +214,119 @@ impl Propagator for NogoodPropagator {
         0
     }
 
+    // fn propagate(&mut self, context: &mut PropagationContextMut) -> PropagationStatusCP {
+    // todo: do incrementally, two levels:
+    // 1. Skip looking at nogoods that are for sure not changing their watched predicate.
+    // For this need to take into account the trail position and when the updates took place.
+    // 2. While going through the watch list, partition the watchers. Then next time we can skip
+    // the ones we looked at before, since those are guaranteed to be not propagating. I think
+    // this only works downwards, need to see how to handle synchronisation/backtracking.
+    //
+    // The idea is to go through nogoods which had their watched predicate evaluate to true.
+    // for update in &self.enqueued_updates {
+    // match update {
+    // Update::LowerBound {
+    // old_lower_bound,
+    // new_lower_bound,
+    // } => {
+    // Go through the watchers of:
+    // 1) Lower bounds in the range (old, new].
+    // 2) Not equals in the range [old, new).
+    // Equality watchers can be skipped since that is a separate domain event.
+    // todo!()
+    // }
+    // Update::UpperBound {
+    // old_upper_bound,
+    // new_upper_bound,
+    // } => {
+    // Go through the watchers of:
+    // 1) Upper bounds in the range [new, old).
+    // 2) Not equals in the range (new, old].
+    // Equality watchers can be skipped since that is a separate domain event.
+    // todo!()
+    // }
+    // Update::Hole { hole_value } => {
+    // Only need to go through the watchers of
+    // not equals for exactly the hole value.
+    // todo!()
+    // }
+    // Update::Assignment { assigned_value } => {
+    // Only need to go through the watchers of
+    // assignments for exactly the assigned value.
+    // todo!()
+    // }
+    // }
+    // }
+    // todo!();
+    // }
+
+    // fn synchronise(&mut self, _context: &PropagationContext) {
+    // self.enqueued_updates.clear();
+    // }
+
+    // fn notify(
+    // &mut self,
+    // _context: &mut PropagationContextMut,
+    // _local_id: LocalId,
+    // _event: OpaqueDomainEvent,
+    // ) -> EnqueueDecision {
+    // self.enqueued_updates.push(value)
+    // EnqueueDecision::Enqueue
+    // It seems we removed the delta idea?
+    // todo!();
+    // }
+
     fn debug_propagate_from_scratch(
         &self,
-        context: &mut PropagationContextMut,
+        _context: &mut PropagationContextMut,
     ) -> PropagationStatusCP {
         // Very inefficient version!
 
         // The algorithm goes through every nogood explicitly
         // and computes from scratch.
-        for nogood in &self.nogoods {
-            self.propagate_nogood(nogood, context)?;
-        }
-        Ok(())
+        // for nogood in self.nogoods.iter() {
+        //    self.debug_propagate_nogood_from_scratch(nogood, context)?;
+        //}
+        todo!();
+        // Ok(())
     }
 
     // Learned nogood during search.
     // Assumption is that it is propagating, and that the propagating predicate is in position [0].
     // Actually the above assumption is not needed in the current version.
-    #[allow(dead_code)]
-    fn hack_add_asserting_nogood(
-        &mut self,
-        nogood: Vec<IntegerPredicate>,
-        // context: &mut PropagationContextMut,
-    ) {
-        self.nogoods.push(nogood);
-        // let result = self.propagate_nogood(&self.nogoods.last().unwrap(), context);
-        // assert!(result.is_ok());
+    // #[allow(dead_code)]
+    // fn hack_add_asserting_nogood(
+    // &mut self,
+    // nogood: Vec<IntegerPredicate>,
+    // context: &mut PropagationContextMut,
+    // ) {
+    // self.debug_propagate_nogood_from_scratch(&nogood, context);
+    //
+    // self.nogoods.push(nogood);
+    // let nogood_id = NogoodId {
+    // id: self.nogoods.len() as u32,
+    // };
+    // self.add_watcher(nogood[0], nogood_id);
+    // self.add_watcher(nogood[1], nogood_id);
+    // self.nogoods.push(nogood);
+    //
+    // TODO PROPAGATION
+    // todo!();
+    // }
+}
+
+#[derive(Default, Clone, Copy, Debug)]
+struct NogoodId {
+    id: u32,
+}
+
+impl StorageKey for NogoodId {
+    fn index(&self) -> usize {
+        self.id as usize
+    }
+
+    fn create_from_index(index: usize) -> Self {
+        NogoodId { id: index as u32 }
     }
 }
 
