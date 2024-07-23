32b372a59204efcd023f2c0cebc4f24b6ee3fa36
Maarten Flippo <maartenflippo@outlook.com>
2024-07-23 11:26:12 +0200

    Actually include the proof files

---
+++ pumpkin-lib/src/proof/mod.rs
@@ -0,0 +1,121 @@
+mod dimacs;
+mod proof_literals;
+
+use std::fs::File;
+use std::num::NonZeroU64;
+use std::path::Path;
+use std::path::PathBuf;
+
+pub use drcp_format::Format;
+use drcp_format::ProofWriter;
+
+use self::dimacs::DimacsProof;
+use self::proof_literals::ProofLiterals;
+use crate::predicates::Predicate;
+use crate::variable_names::VariableNames;
+#[cfg(doc)]
+use crate::Solver;
+
+/// A proof log which logs the proof steps necessary to prove unsatisfiability or optimality. We
+/// allow the following types of proofs:
+/// - A CP proof log - This can be created using [`ProofLog::cp`].
+/// - A DIMACS proof log - This can be created using [`ProofLog::dimacs`].
+///
+/// When a proof log should not be generated, use the implementation of [`Default`].
+#[derive(Debug, Default)]
+pub struct ProofLog {
+    internal_proof: Option<ProofImpl>,
+}
+
+impl ProofLog {
+    /// Create a CP proof logger.
+    pub fn cp(file_path: &Path, format: Format) -> std::io::Result<ProofLog> {
+        let definitions_path = file_path.with_extension("lits");
+        let file = File::create(file_path)?;
+
+        let writer = ProofWriter::new(format, file, ProofLiterals::default());
+
+        Ok(ProofLog {
+            internal_proof: Some(ProofImpl::CpProof {
+                writer,
+                definitions_path,
+            }),
+        })
+    }
+
+    /// Create a dimacs proof logger.
+    pub fn dimacs(file_path: &Path) -> std::io::Result<ProofLog> {
+        let file = File::create(file_path)?;
+        Ok(ProofLog {
+            internal_proof: Some(ProofImpl::DimacsProof(DimacsProof::new(file))),
+        })
+    }
+
+    /// Log a learned clause to the proof.
+    pub(crate) fn log_learned_clause(
+        &mut self,
+        literals: impl IntoIterator<Item = Predicate>,
+        variable_names: &VariableNames,
+    ) -> std::io::Result<NonZeroU64> {
+        // Used as a proof clause ID when no proof log is used. This should ideally be a `const`,
+        // but `Option::<T>::unwrap()` is not yet stable in const context.
+        let default_clause_id: NonZeroU64 = NonZeroU64::new(1).unwrap();
+
+        match &mut self.internal_proof {
+            Some(ProofImpl::CpProof { writer, .. }) => writer.log_nogood_clause(literals),
+
+            Some(ProofImpl::DimacsProof(writer)) => writer.learned_clause(literals, variable_names),
+
+            None => Ok(default_clause_id),
+        }
+    }
+
+    pub(crate) fn unsat(self, variable_names: &VariableNames) -> std::io::Result<()> {
+        match self.internal_proof {
+            Some(ProofImpl::CpProof {
+                writer,
+                definitions_path,
+            }) => {
+                let literals = writer.unsat()?;
+                let file = File::create(definitions_path)?;
+                literals.write(file, variable_names)
+            }
+            Some(ProofImpl::DimacsProof(mut writer)) => writer
+                .learned_clause(std::iter::empty(), variable_names)
+                .map(|_| ()),
+            None => Ok(()),
+        }
+    }
+
+    pub(crate) fn optimal(
+        self,
+        objective_bound: Predicate,
+        variable_names: &VariableNames,
+    ) -> std::io::Result<()> {
+        match self.internal_proof {
+            Some(ProofImpl::CpProof {
+                writer,
+                definitions_path,
+            }) => {
+                let literals = writer.optimal(objective_bound)?;
+                let file = File::create(definitions_path)?;
+                literals.write(file, variable_names)
+            }
+
+            Some(ProofImpl::DimacsProof(_)) => {
+                panic!("Cannot conclude optimality in DIMACS proof")
+            }
+
+            None => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum ProofImpl {
+    CpProof {
+        writer: ProofWriter<File, ProofLiterals>,
+        definitions_path: PathBuf,
+    },
+    DimacsProof(DimacsProof<File>),
+}
---
+++ pumpkin-lib/src/proof/proof_literals.rs
@@ -0,0 +1,95 @@
+use std::io::Write;
+use std::num::NonZeroI32;
+use std::num::NonZeroU32;
+
+use drcp_format::AtomicConstraint;
+use drcp_format::Comparison;
+use drcp_format::IntAtomicConstraint;
+use drcp_format::LiteralCodeProvider;
+use drcp_format::LiteralDefinitions;
+
+use crate::basic_types::HashMap;
+use crate::predicates::Predicate;
+use crate::variable_names::VariableNames;
+
+#[derive(Default, Debug)]
+pub(crate) struct ProofLiterals {
+    /// All the predicates seen in the proof log.
+    ///
+    /// The predicates in the map are only LessThanEqual or Equal. The other variants are negations
+    /// of the predicates in the map.
+    variables: HashMap<Predicate, NonZeroU32>,
+}
+
+impl ProofLiterals {
+    pub(crate) fn write(
+        self,
+        sink: impl Write,
+        variable_names: &VariableNames,
+    ) -> std::io::Result<()> {
+        let mut definitions = LiteralDefinitions::default();
+
+        for (predicate, code) in self.variables.into_iter() {
+            let proof_atomic = predicate_to_atomic(predicate, variable_names);
+            definitions.add(code, proof_atomic);
+        }
+
+        definitions.write(sink)
+    }
+}
+
+fn predicate_to_atomic(
+    predicate: Predicate,
+    variable_names: &VariableNames,
+) -> AtomicConstraint<'_> {
+    match predicate {
+        Predicate::UpperBound {
+            domain_id,
+            upper_bound,
+        } => AtomicConstraint::Int(IntAtomicConstraint {
+            name: variable_names
+                .get_int_name(domain_id)
+                .expect("integer domain is unnamed"),
+            comparison: Comparison::LessThanEqual,
+            value: upper_bound.into(),
+        }),
+        Predicate::Equal {
+            domain_id,
+            equality_constant,
+        } => AtomicConstraint::Int(IntAtomicConstraint {
+            name: variable_names
+                .get_int_name(domain_id)
+                .expect("integer domain is unnamed"),
+            comparison: Comparison::Equal,
+            value: equality_constant.into(),
+        }),
+
+        Predicate::NotEqual { .. } | Predicate::LowerBound { .. } => {
+            panic!("Only Equal and UpperBound predicates should be in the literal definition")
+        }
+    }
+}
+
+impl LiteralCodeProvider for ProofLiterals {
+    type Literal = Predicate;
+
+    fn to_code(&mut self, literal: Self::Literal) -> NonZeroI32 {
+        let key = match literal {
+            l @ (Predicate::UpperBound { .. } | Predicate::Equal { .. }) => l,
+            l @ (Predicate::LowerBound { .. } | Predicate::NotEqual { .. }) => !l,
+        };
+
+        let next_code = NonZeroU32::new(self.variables.len() as u32 + 1).unwrap();
+        let code = *self.variables.entry(key).or_insert(next_code);
+
+        let code: NonZeroI32 = code
+            .try_into()
+            .expect("cannot handle more than i32::MAX literal codes");
+
+        if key == literal {
+            code
+        } else {
+            -code
+        }
+    }
+}
