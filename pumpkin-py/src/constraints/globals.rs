7d57f6c95e35e926abf0234769e2ce86ae07a77e
Maarten Flippo <maartenflippo@outlook.com>
2024-09-13 12:51:23 +0200

    Declare macro to generate python types for constraints

--- pumpkin-py/src/constraints/globals.rs
+++ pumpkin-py/src/constraints/globals.rs
@@ -4,36 +4,56 @@ use pyo3::pymethods;
 
 use crate::core::Variable;
 
-/// All variables have distinct values.
-#[pyclass]
-#[derive(Clone)]
-pub struct AllDifferent(Vec<Variable>);
+macro_rules! python_constraint {
+    ($name:ident : $constraint_func:ident { $($field:ident : $type:ty),+ $(,)? }) => {
+        #[pyclass]
+        #[derive(Clone)]
+        pub struct $name {
+            $($field: $type),+
+        }
 
-#[pymethods]
-impl AllDifferent {
-    #[new]
-    fn new(variables: Vec<Variable>) -> Self {
-        AllDifferent(variables)
+        #[pymethods]
+        impl $name {
+            #[new]
+            fn new($($field: $type),+) -> Self {
+                $name {
+                    $($field),+
+                }
+            }
+        }
+
+        impl pumpkin_lib::constraints::Constraint for $name {
+            fn post(
+                self,
+                solver: &mut pumpkin_lib::Solver,
+                tag: Option<std::num::NonZero<u32>>,
+            ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
+                constraints::$constraint_func($(self.$field),+).post(solver, tag)
+            }
+
+            fn implied_by(
+                self,
+                solver: &mut pumpkin_lib::Solver,
+                reification_literal: pumpkin_lib::variables::Literal,
+                tag: Option<std::num::NonZero<u32>>,
+            ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
+                constraints::$constraint_func($(self.$field),+).implied_by(solver, reification_literal, tag)
+            }
+        }
+    };
+}
+
+python_constraint! {
+    AllDifferent: all_different {
+        variables: Vec<Variable>,
     }
 }
 
-impl pumpkin_lib::constraints::Constraint for AllDifferent {
-    fn post(
-        self,
-        solver: &mut pumpkin_lib::Solver,
-        tag: Option<std::num::NonZero<u32>>,
-    ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
-        let variables = self.0.into_iter().map(|var| var.0).collect::<Vec<_>>();
-        constraints::all_different(variables).post(solver, tag)
-    }
-
-    fn implied_by(
-        self,
-        solver: &mut pumpkin_lib::Solver,
-        reification_literal: pumpkin_lib::variables::Literal,
-        tag: Option<std::num::NonZero<u32>>,
-    ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
-        let variables = self.0.into_iter().map(|var| var.0).collect::<Vec<_>>();
-        constraints::all_different(variables).implied_by(solver, reification_literal, tag)
+python_constraint! {
+    Cumulative: cumulative {
+        start_times: Vec<Variable>,
+        durations: Vec<i32>,
+        resource_requirements: Vec<i32>,
+        resource_capacity: i32,
     }
 }
