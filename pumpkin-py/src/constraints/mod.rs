a9cd5b2b459ac2c0cd59647c1d910048556f3321
Maarten Flippo <maartenflippo@outlook.com>
2024-09-13 12:56:50 +0200

    Extract constraint union definition into macro

--- pumpkin-py/src/constraints/mod.rs
+++ pumpkin-py/src/constraints/mod.rs
@@ -1,35 +1,44 @@
 mod globals;
 
-use globals::AllDifferent;
+use globals::*;
 use pyo3::prelude::*;
 
-#[derive(Clone, FromPyObject)]
-pub enum Constraint {
-    AllDifferent(AllDifferent),
-}
-
-impl pumpkin_lib::constraints::Constraint for Constraint {
-    fn post(
-        self,
-        solver: &mut pumpkin_lib::Solver,
-        tag: Option<std::num::NonZero<u32>>,
-    ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
-        match self {
-            Constraint::AllDifferent(constraint) => constraint.post(solver, tag),
+macro_rules! declare_constraint_enum {
+    ($name:ident { $($constraint:ident),+ $(,)? }) => {
+        #[derive(Clone, FromPyObject)]
+        pub enum $name {
+            $($constraint($constraint)),+
         }
-    }
 
-    fn implied_by(
-        self,
-        solver: &mut pumpkin_lib::Solver,
-        reification_literal: pumpkin_lib::variables::Literal,
-        tag: Option<std::num::NonZero<u32>>,
-    ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
-        match self {
-            Constraint::AllDifferent(constraint) => {
-                constraint.implied_by(solver, reification_literal, tag)
+        impl pumpkin_lib::constraints::Constraint for Constraint {
+            fn post(
+                self,
+                solver: &mut pumpkin_lib::Solver,
+                tag: Option<std::num::NonZero<u32>>,
+            ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
+                match self {
+                    $($name::$constraint(cns) => cns.post(solver, tag)),+
+                }
+            }
+
+            fn implied_by(
+                self,
+                solver: &mut pumpkin_lib::Solver,
+                reification_literal: pumpkin_lib::variables::Literal,
+                tag: Option<std::num::NonZero<u32>>,
+            ) -> Result<(), pumpkin_lib::ConstraintOperationError> {
+                match self {
+                    $($name::$constraint(cns) => cns.implied_by(solver, reification_literal, tag)),+
+                }
             }
         }
+    };
+}
+
+declare_constraint_enum! {
+    Constraint {
+        AllDifferent,
+        Cumulative,
     }
 }
 
