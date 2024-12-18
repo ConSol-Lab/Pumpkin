b9f3c9a234352f2d95a27935b212977df998d632
Konstantin Sidorov <k.sidorov@tudelft.nl>
2024-12-18 02:29:37 +0100

    SCIP model encoding + edge propagation

--- pumpkin-solver/src/propagators/cumulative/node_packing.rs
+++ pumpkin-solver/src/propagators/cumulative/node_packing.rs
@@ -1,9 +1,15 @@
+use core::f64;
 use std::cmp::min;
 use std::collections::VecDeque;
 use std::rc::Rc;
 use std::usize;
 
 use itertools::Itertools;
+use russcip::Model;
+use russcip::ObjSense;
+use russcip::ProblemOrSolving;
+use russcip::WithSolutions;
+use russcip::WithSolvingStats;
 
 use crate::basic_types::moving_averages::CumulativeMovingAverage;
 use crate::basic_types::moving_averages::MovingAverage;
@@ -74,158 +80,162 @@ impl<Var: IntegerVariable + Clone + 'static> NodePackingPropagator<Var> {
         self.parameters.tasks.to_vec().into()
     }
 
-    fn remove_compatible(
-        &self,
-        context: PropagationContext,
-        activities: &mut VecDeque<Rc<Task<Var>>>,
-        selected_activity: Rc<Task<Var>>,
-    ) {
-        activities.retain(|activity| {
-            // `activity` and `selected_activity` must either be statically incompatible
-            // or have disjoint time spans for execution
-            self.parameters.static_incompatibilities[selected_activity.id.unpack() as usize]
-                [activity.id.unpack() as usize]
-                || !(context.lower_bound(&activity.start_variable)
-                    <= context.upper_bound(&selected_activity.start_variable)
-                        + selected_activity.processing_time
-                    && context.lower_bound(&selected_activity.start_variable)
-                        <= context.upper_bound(&activity.start_variable) + activity.processing_time)
-        })
-    }
-
-    fn cover_size(intervals: &mut [(i32, i32)]) -> i64 {
-        let mut cover = 0;
-        intervals.sort();
-        let mut lo = 0;
-        let mut hi = 0;
-        for (a, b) in intervals {
-            assert!(*a >= 0);
-            assert!(*b >= 0);
-            if *a > hi {
-                cover += hi as i64;
-                cover -= lo as i64;
-                lo = *a;
-            }
-            if *b > hi {
-                hi = *b;
-            }
-        }
-        cover += hi as i64;
-        cover -= lo as i64;
-        cover
-    }
-
-    fn score_violation(
-        durations: &[i32],
-        intervals: &[(i32, i32)],
-        clique: &[usize]
-    ) -> i64 {
-        Self::cover_size(&mut clique.iter().map(|&ix| intervals[ix]).collect_vec()) -
-            clique.iter().map(|&ix| durations[ix] as i64).sum::<i64>() + 1
-    }
-
-    fn score(
-        clique: &[usize],
-        durations: &[i32],
-        intervals: &[(i32, i32)],
-        lambda: f64
-    ) -> f64 {
-        clique.iter().map(|&ix| durations[ix] as f64 - 1.0 / lambda).sum::<f64>() - 
-            Self::cover_size(&mut clique.iter().map(|&ix| intervals[ix]).collect_vec()) as f64
-    }
-
-    fn is_clique(&self, clique: &[usize], intervals: &[(i32, i32)]) -> bool {
-        if !clique.is_empty() {
-            for &a in clique {
-                for &b in clique {
-                    if a == b { continue; }
-                    if self.parameters.static_incompatibilities[a][b] { continue; }
-                    if !(intervals[a].0 <= intervals[b].1 && intervals[b].0 <= intervals[a].1) { continue; }
-                    return false;
-                }
-            }
-            true
-        } else {
-            // HACK Empty clique *is* a clique but the LS seems to fall into it too hastily
-            false
-        }
-    }
-
-    fn accept(
-        &self,
-        new_clique: &[usize],
-        ref_clique: &[usize],
-        durations: &[i32],
-        intervals: &[(i32, i32)],
-        lambda: f64
-    ) -> bool {
-        self.is_clique(new_clique, intervals) && (
-            Self::score(new_clique, durations, intervals, lambda) >
-            Self::score(ref_clique, durations, intervals, lambda)
-        )
-    }
-
-    fn find_conflict(
-        &self,
-        context: PropagationContext<'_>,
-    ) -> Option<Vec<Rc<Task<Var>>>> {
+    fn find_conflict(&self, context: PropagationContext<'_>) -> Option<Vec<Rc<Task<Var>>>> {
         let all_tasks = self.create_initial_activity_list();
-        let durations = all_tasks.iter().map(|task| task.processing_time).collect_vec();
-        let intervals = all_tasks.iter().map(|task| (
-            context.lower_bound(&task.start_variable),
-            context.upper_bound(&task.start_variable) + task.processing_time,
-        )).collect_vec();
-        let mut curr_lambda = 0.1 * durations.iter().map(|&d| 1.0 / d as f64).reduce(f64::max).unwrap_or(0.0);
-        let mut curr_clique = vec![
-            (0..all_tasks.len())
-                .max_by_key(|&task_ix| durations[task_ix] + intervals[task_ix].1 - intervals[task_ix].0)
-                .expect("Empty task list")
-        ];
-        let mut best_clique: Option<Vec<usize>> = None;
-        for ls_iter in 1..=100 {
-            // Enumerate all candidates for extending the clique
-            'ls: for add_vertex in 0..all_tasks.len() {
-                if curr_clique.contains(&add_vertex) {
-                    continue;
+        let durations = all_tasks
+            .iter()
+            .map(|task| task.processing_time)
+            .collect_vec();
+        let intervals = all_tasks
+            .iter()
+            .map(|task| {
+                (
+                    context.lower_bound(&task.start_variable),
+                    context.upper_bound(&task.start_variable) + task.processing_time,
+                )
+            })
+            .collect_vec();
+        // Try finding a conflicting *pair* of tasks
+        for (index_lhs, (duration_lhs, (start_lhs, finish_lhs))) in
+            durations.iter().zip(&intervals).enumerate()
+        {
+            for (index_rhs, (duration_rhs, (start_rhs, finish_rhs))) in
+                durations.iter().take(index_lhs).zip(&intervals).enumerate()
+            {
+                if self.parameters.static_incompatibilities[index_lhs][index_rhs]
+                    && duration_lhs + duration_rhs
+                        > finish_rhs.max(finish_lhs) - start_rhs.min(start_lhs)
+                {
+                    // eprintln!("Trivial clique {index_lhs} {index_rhs}");
+                    return Some(
+                        [index_lhs, index_rhs]
+                            .iter()
+                            .map(|&ix| all_tasks[ix].clone())
+                            .collect(),
+                    );
                 }
-                let mut add_clique = curr_clique.clone();
-                add_clique.push(add_vertex);
-                if self.accept(&add_clique, &curr_clique, &durations, &intervals, curr_lambda) {
-                    eprintln!("{ls_iter} {curr_clique:?} ADD {add_vertex}");
-                    curr_clique = add_clique;
-                    break;
-                }
-                for rem_vertex in 0..all_tasks.len() {
-                    if !curr_clique.contains(&rem_vertex) {
-                        continue;
-                    }
-                    let mut rem_clique = curr_clique.clone();
-                    rem_clique.retain(|&x| x != rem_vertex);
-                    if self.accept(&rem_clique, &curr_clique, &durations, &intervals, curr_lambda) {
-                        eprintln!("{ls_iter} {curr_clique:?} REM {rem_vertex}");
-                        curr_clique = rem_clique;
-                        break 'ls; 
-                    }
-                    let mut swap_clique = rem_clique.clone();
-                    swap_clique.push(add_vertex);
-                    if self.accept(&swap_clique, &curr_clique, &durations, &intervals, curr_lambda) {
-                        eprintln!("{ls_iter} {curr_clique:?} SWAP {add_vertex} {rem_vertex}");
-                        curr_clique = swap_clique;
-                        break 'ls; 
-                    }
-                }
-            }
-            // Update the Lagrangian multiplier
-            let violation = Self::score_violation(&durations, &intervals, &curr_clique);
-            curr_lambda += 0.1 * (violation as f64);
-            eprintln!("{ls_iter} {curr_clique:?} {curr_lambda} {violation}");
-            if curr_lambda < 0.0 { curr_lambda = 0.0; }
-            // If possible, store a better valid clique
-            if violation <= 0 && curr_clique.len() < best_clique.as_ref().map(|x| x.len()).unwrap_or(usize::MAX) {
-                best_clique = Some(curr_clique.clone());
             }
         }
-        best_clique.map(|indices: Vec<usize>| indices.iter().map(|&ix| all_tasks[ix].clone()).collect())
+        return None;
+        // Decompose the problem into the one of finding the conflict that fits into
+        // an interval [x..y), with x and y being the start/finish points for some of the intervals
+        let mut time_points = intervals.iter().flat_map(|x| [x.0, x.1]).collect_vec();
+        time_points.sort();
+        time_points.dedup();
+        let mut best_clique: Option<Vec<usize>> = None;
+        let mut candidate_intervals = (0..time_points.len())
+            .flat_map(|rhs_index| {
+                let rhs = time_points[rhs_index];
+                let all_tasks = all_tasks.clone();
+                let intervals = intervals.clone();
+                let durations = durations.clone();
+                time_points[0..rhs_index].iter().filter_map(move |&lhs| {
+                    let total_duration = (0..all_tasks.len())
+                        .filter(|&ix| lhs <= intervals[ix].0 && intervals[ix].1 <= rhs)
+                        .map(|ix| durations[ix])
+                        .sum::<i32>();
+                    if total_duration > rhs - lhs {
+                        Some((lhs, rhs))
+                    } else {
+                        None
+                    }
+                })
+            })
+            .collect_vec();
+        candidate_intervals.sort_by_key(|(lhs, rhs)| {
+            -(0..all_tasks.len())
+                .filter(|&ix| *lhs <= intervals[ix].0 && intervals[ix].1 <= *rhs)
+                .map(|ix| durations[ix])
+                .sum::<i32>()
+        });
+        for (lhs, rhs) in candidate_intervals.iter().take(5).cloned() {
+            // for rhs_index in 0..time_points.len() {
+            //     let rhs = time_points[rhs_index];
+            //     for &lhs in time_points[0..rhs_index].iter() {
+            // Filter the fitting intervals and construct the clique-finding MIP
+            let indices = (0..all_tasks.len())
+                .filter(|&ix| lhs <= intervals[ix].0 && intervals[ix].1 <= rhs)
+                .collect_vec();
+            let mut model = Model::new()
+                .hide_output()
+                .include_default_plugins()
+                .create_prob("conflict_discovery")
+                .set_obj_sense(ObjSense::Minimize);
+            // Binary variables encoding interval selection
+            let vars = indices
+                .iter()
+                .map(|ix| {
+                    model.add_var(
+                        0.,
+                        1.,
+                        1.,
+                        format!("x{ix}").as_str(),
+                        russcip::VarType::Binary,
+                    )
+                })
+                .collect_vec();
+            // Duration bound
+            model.add_cons(
+                vars.clone(),
+                &indices.iter().map(|&ix| durations[ix] as f64).collect_vec(),
+                (rhs - lhs + 1) as f64,
+                f64::INFINITY,
+                "duration",
+            );
+            // Clique constraints
+            for (j, &b) in indices.iter().enumerate() {
+                for (i, &a) in indices[0..j].iter().enumerate() {
+                    if self.parameters.static_incompatibilities[a][b] {
+                        // Static incompatible tasks, do not constrain
+                    }
+                    // else if intervals[a].0.max(intervals[b].0)
+                    //     >= intervals[a].1.min(intervals[b].1)
+                    // {
+                    //     // Disjoint intervals, do not constrain
+                    // }
+                    else {
+                        // Forbid choosing both intervals
+                        model.add_cons(
+                            vec![vars[i].clone(), vars[j].clone()],
+                            &[1.0, 1.0],
+                            0.,
+                            1.,
+                            format!("clique_{}_{}", a, b).as_str(),
+                        );
+                    }
+                }
+            }
+            let solved_model = model.solve();
+            let mut new_clique = vec![];
+            match solved_model.status() {
+                russcip::Status::Optimal => {
+                    // An optimal clique is discovered, store it
+                    let sol = solved_model.best_sol().unwrap();
+                    for (i, var) in vars.iter().enumerate() {
+                        if sol.val(var.clone()) > 1e-3 {
+                            new_clique.push(indices[i]);
+                        }
+                    }
+                    // eprintln!("{lhs}..{rhs} {new_clique:?}");
+                    if new_clique.len()
+                        < best_clique.as_ref().map(|x| x.len()).unwrap_or(usize::MAX)
+                    {
+                        best_clique = Some(new_clique);
+                    }
+                }
+                russcip::Status::Infeasible => {
+                    // No conflict exists, keep going
+                }
+                _ => {
+                    // Miscellaneous reason, stay away
+                }
+            }
+        }
+        if let Some(indices) = best_clique.as_ref() {
+            eprintln!("The best clique is {indices:?}");
+        };
+        best_clique
+            .map(|indices: Vec<usize>| indices.iter().map(|&ix| all_tasks[ix].clone()).collect())
     }
 }
 
@@ -242,23 +252,31 @@ impl<Var: IntegerVariable + 'static> Propagator for NodePackingPropagator<Var> {
         self.statistics.n_calls += 1;
         if let Some(clique) = self.find_conflict(context.as_readonly()) {
             self.statistics.n_conflicts += 1;
-            self.statistics.average_clique_size.add_term(clique.len() as u64);
-            let est = clique.iter().map(|task| context.lower_bound(&task.start_variable)).min().expect("Empty clique");
-            let lft = clique.iter().map(|task| context.upper_bound(&task.start_variable) + task.processing_time).max().expect("Empty clique");
-            eprintln!("Tasks {clique:?} do not fit in [{est},{lft}]");
-            Err(
-                crate::basic_types::Inconsistency::Conflict(
-                    clique
-                        .iter()
-                        .flat_map(|task| {
-                            [
-                                predicate!(task.start_variable >= est),
-                                predicate!(task.start_variable <= lft - task.processing_time),
-                            ]
-                        })
-                        .collect()
-                )
-            )
+            self.statistics
+                .average_clique_size
+                .add_term(clique.len() as u64);
+            let est = clique
+                .iter()
+                .map(|task| context.lower_bound(&task.start_variable))
+                .min()
+                .expect("Empty clique");
+            let lft = clique
+                .iter()
+                .map(|task| context.upper_bound(&task.start_variable) + task.processing_time)
+                .max()
+                .expect("Empty clique");
+            // eprintln!("Tasks {clique:?} do not fit in [{est},{lft}]");
+            Err(crate::basic_types::Inconsistency::Conflict(
+                clique
+                    .iter()
+                    .flat_map(|task| {
+                        [
+                            predicate!(task.start_variable >= est),
+                            predicate!(task.start_variable <= lft - task.processing_time),
+                        ]
+                    })
+                    .collect(),
+            ))
         } else {
             Ok(())
         }
@@ -269,21 +287,27 @@ impl<Var: IntegerVariable + 'static> Propagator for NodePackingPropagator<Var> {
         mut context: PropagationContextMut,
     ) -> PropagationStatusCP {
         if let Some(clique) = self.find_conflict(context.as_readonly()) {
-            let est = clique.iter().map(|task| context.lower_bound(&task.start_variable)).min().expect("Empty clique");
-            let lst = clique.iter().map(|task| context.upper_bound(&task.start_variable)).max().expect("Empty clique");
-            Err(
-                crate::basic_types::Inconsistency::Conflict(
-                    clique
-                        .iter()
-                        .flat_map(|task| {
-                            [
-                                predicate!(task.start_variable >= est),
-                                predicate!(task.start_variable <= lst),
-                            ]
-                        })
-                        .collect()
-                )
-            )
+            let est = clique
+                .iter()
+                .map(|task| context.lower_bound(&task.start_variable))
+                .min()
+                .expect("Empty clique");
+            let lst = clique
+                .iter()
+                .map(|task| context.upper_bound(&task.start_variable))
+                .max()
+                .expect("Empty clique");
+            Err(crate::basic_types::Inconsistency::Conflict(
+                clique
+                    .iter()
+                    .flat_map(|task| {
+                        [
+                            predicate!(task.start_variable >= est),
+                            predicate!(task.start_variable <= lst),
+                        ]
+                    })
+                    .collect(),
+            ))
         } else {
             Ok(())
         }
