50b0e2024dd49b03cc695e01ba1e5978f7ce289e
Imko Marijnissen <50290518+ImkoMarijnissen@users.noreply.github.com>
2025-01-15 14:16:55 +0100

    fix: alternating brancher did not call the methods correctly (#128)
    The `AlternatingBrancher` did not call the correct methods on the
    default brancher meaning that the solver had poor performance during
    free search (since it did not restore any keys to the heap upon
    backtracking). This has been fixed by performing the right calls

--- pumpkin-solver/src/engine/cp/assignments.rs
+++ pumpkin-solver/src/engine/cp/assignments.rs
@@ -904,7 +904,7 @@ impl IntegerDomain {
         self.lower_bound_updates
             .iter()
             .filter(|u| u.trail_position <= trail_position)
-            .last()
+            .next_back()
             .expect("Cannot fail")
             .bound
     }
@@ -945,7 +945,7 @@ impl IntegerDomain {
         self.upper_bound_updates
             .iter()
             .filter(|u| u.trail_position <= trail_position)
-            .last()
+            .next_back()
             .expect("Cannot fail")
             .bound
     }
