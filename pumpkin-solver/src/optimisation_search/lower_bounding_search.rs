d5cb74dbe8355fab57b871513d52af678c556a3f
ImkoMarijnissen <imko.marijnissen@gmail.com>
2025-01-22 09:24:15 +0100

    refactor: remove redundant variable

--- pumpkin-solver/src/optimisation_search/lower_bounding_search.rs
+++ pumpkin-solver/src/optimisation_search/lower_bounding_search.rs
@@ -27,11 +27,6 @@ impl OptimisationProcedure for LowerBoundingSearch {
         // objective_multiplier ensures that the objective is correctly logged.
         let objective_multiplier = if is_maximising { -1 } else { 1 };
 
-        // We keep track of the lower-bound on the variable
-        let mut lower_bound = solver
-            .satisfaction_solver
-            .get_lower_bound(&objective_variable);
-
         // First we do a feasibility check
         let feasibility_check = solver.satisfaction_solver.solve(termination, brancher);
         match feasibility_check {
@@ -63,32 +58,16 @@ impl OptimisationProcedure for LowerBoundingSearch {
         loop {
             solver.satisfaction_solver.restore_state_at_root(brancher);
 
-            // We know that the objective value should be at least the value which we are currently
-            // attempting
-            //
-            // In the first iteration, this will add a trivial constraint
-            //
-            // In all subsequent iterations, if infeasibility is detected, then it will set the
-            // lower-bound of the objective variable to be +1 of the last value attempted)
-            let _ = solver.add_clause([predicate!(objective_variable >= lower_bound)]);
-
-            // It could be the case that due to learning and/or propagation we have learned that
-            // the lower-bound of the variable is already larger; we take this into account by
-            // updating the variable
-            lower_bound = lower_bound.max(
-                solver
-                    .satisfaction_solver
-                    .get_lower_bound(&objective_variable),
-            );
+            let assumption =
+                predicate!(objective_variable <= solver.lower_bound(&objective_variable));
 
             info!(
-                "LUS - Attempt to find solution with {}",
-                predicate!(objective_variable <= lower_bound)
+                "Lower-Bounding Search - Attempting to find solution with assumption {assumption}"
             );
 
             // Solve under the assumption that the objective variable is lower than `lower-bound`
             let solve_result = solver.satisfaction_solver.solve_under_assumptions(
-                &[predicate!(objective_variable <= lower_bound)],
+                &[assumption],
                 termination,
                 brancher,
             );
@@ -126,8 +105,9 @@ impl OptimisationProcedure for LowerBoundingSearch {
                     return OptimisationResult::Optimal(best_solution);
                 }
                 CSPSolverExecutionFlag::Infeasible => {
-                    // The lower-bound is still too small, increase it until we find SAT
-                    lower_bound += 1;
+                    // We add the (hard) constraint that the negated assumption should hold (i.e.,
+                    // the solution should be at least as large as the found solution)
+                    let _ = solver.add_clause([!assumption]);
                 }
                 CSPSolverExecutionFlag::Timeout => {
                     // Reset the state whenever we return a result
