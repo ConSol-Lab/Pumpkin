1c4b85fe9fb802c9409df52741c0dff8c6b68d4a
Imko Marijnissen <50290518+ImkoMarijnissen@users.noreply.github.com>
2025-02-03 14:55:00 +0100

    feat: adding random variable selector + sparse_set fixes (#139)
    Profiling has shown that the variable/value selectors are oftentimes too
    slow due to the fact that they need to find the minimum/maximum value
    over some condition.
    
    It is also currently unclear what would be a good backup strategy for
    VSIDS; @maartenflippo suggested that a fully random strategy would
    likely be a good idea. To this end, this PR aims to implement a random
    variable selector which keeps track of the variables using a sparse set
    and a value selector which picks a random value within the lower-bound
    and upper-bound and randomly splits the domain on that value.
    
    The PR sets this as the default backup strategy.
    
    _The PR also includes some small fixes to the sparse set_
    
    _Edit: Due to failing test cases, this PR is now dependent on #136_

---
+++ pumpkin-solver/src/branching/variable_selection/random.rs
@@ -0,0 +1,152 @@
+use super::VariableSelector;
+use crate::branching::SelectionContext;
+use crate::containers::SparseSet;
+use crate::containers::StorageKey;
+use crate::variables::DomainId;
+
+/// A [`VariableSelector`] which selects a random unfixed variable.
+#[derive(Debug)]
+pub struct RandomSelector {
+    variables: SparseSet<DomainId>,
+}
+
+impl RandomSelector {
+    pub fn new(variables: impl IntoIterator<Item = DomainId>) -> Self {
+        // Note the -1 due to the fact that the indices of the domain ids start at 1
+        Self {
+            variables: SparseSet::new(variables.into_iter().collect(), |element| {
+                element.index() - 1
+            }),
+        }
+    }
+}
+
+impl VariableSelector<DomainId> for RandomSelector {
+    fn select_variable(&mut self, context: &mut SelectionContext) -> Option<DomainId> {
+        if self.variables.is_empty() {
+            return None;
+        }
+
+        let mut variable = *self.variables.get(
+            context
+                .random()
+                .generate_usize_in_range(0..self.variables.len()),
+        );
+
+        while context.is_integer_fixed(variable) {
+            self.variables.remove_temporarily(&variable);
+            if self.variables.is_empty() {
+                return None;
+            }
+
+            variable = *self.variables.get(
+                context
+                    .random()
+                    .generate_usize_in_range(0..self.variables.len()),
+            );
+        }
+
+        Some(variable)
+    }
+
+    fn on_unassign_integer(&mut self, variable: DomainId, _value: i32) {
+        self.variables.insert(variable);
+    }
+
+    fn is_restart_pointless(&mut self) -> bool {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::basic_types::tests::TestRandom;
+    use crate::branching::variable_selection::RandomSelector;
+    use crate::branching::variable_selection::VariableSelector;
+    use crate::branching::SelectionContext;
+
+    #[test]
+    fn test_selects_randomly() {
+        let assignments = SelectionContext::create_for_testing(vec![(0, 10), (5, 20), (1, 3)]);
+        let mut test_rng = TestRandom {
+            usizes: vec![1],
+            ..Default::default()
+        };
+        let integer_variables = assignments.get_domains().collect::<Vec<_>>();
+        let mut strategy = RandomSelector::new(assignments.get_domains());
+
+        let mut context = SelectionContext::new(&assignments, &mut test_rng);
+
+        let selected = strategy.select_variable(&mut context);
+        assert!(selected.is_some());
+        assert_eq!(selected.unwrap(), integer_variables[1]);
+    }
+
+    #[test]
+    fn test_selects_randomly_not_unfixed() {
+        let assignments = SelectionContext::create_for_testing(vec![(0, 10), (5, 5), (1, 3)]);
+        let mut test_rng = TestRandom {
+            usizes: vec![1, 0],
+            ..Default::default()
+        };
+        let integer_variables = assignments.get_domains().collect::<Vec<_>>();
+        let mut strategy = RandomSelector::new(assignments.get_domains());
+
+        let mut context = SelectionContext::new(&assignments, &mut test_rng);
+
+        let selected = strategy.select_variable(&mut context);
+        assert!(selected.is_some());
+        assert_eq!(selected.unwrap(), integer_variables[0]);
+    }
+
+    #[test]
+    fn test_select_nothing_if_all_fixed() {
+        let assignments = SelectionContext::create_for_testing(vec![(0, 0), (5, 5), (1, 1)]);
+        let mut test_rng = TestRandom {
+            usizes: vec![1, 0, 0],
+            ..Default::default()
+        };
+        let mut strategy = RandomSelector::new(assignments.get_domains());
+
+        let mut context = SelectionContext::new(&assignments, &mut test_rng);
+
+        let selected = strategy.select_variable(&mut context);
+        assert!(selected.is_none());
+    }
+
+    #[test]
+    fn test_select_unfixed_variable_after_fixing() {
+        let mut assignments = SelectionContext::create_for_testing(vec![(0, 0), (5, 7), (1, 1)]);
+        let mut test_rng = TestRandom {
+            usizes: vec![2, 0, 0, 0, 0],
+            ..Default::default()
+        };
+        let integer_variables = assignments.get_domains().collect::<Vec<_>>();
+        let mut strategy = RandomSelector::new(assignments.get_domains());
+
+        {
+            let mut context = SelectionContext::new(&assignments, &mut test_rng);
+
+            let selected = strategy.select_variable(&mut context);
+            assert!(selected.is_some());
+            assert_eq!(selected.unwrap(), integer_variables[1]);
+        }
+
+        assignments.increase_decision_level();
+        let _ = assignments.tighten_lower_bound(integer_variables[1], 7, None);
+
+        {
+            let mut context = SelectionContext::new(&assignments, &mut test_rng);
+
+            let selected = strategy.select_variable(&mut context);
+            assert!(selected.is_none());
+        }
+
+        let _ = assignments.synchronise(0, 0, false);
+        strategy.on_unassign_integer(integer_variables[1], 7);
+        let mut context = SelectionContext::new(&assignments, &mut test_rng);
+        let selected = strategy.select_variable(&mut context);
+        assert!(selected.is_some());
+        assert_eq!(selected.unwrap(), integer_variables[1]);
+    }
+}
