ea69cf4f965d3dd37dadb870a7ef269a1cf85e24
Maarten Flippo <maartenflippo@outlook.com>
2025-03-10 13:30:05 +0100

    feat: enable proof logging in appropriate test cases (#154)
    Only logs the proof; does not check at the moment

--- pumpkin-solver/tests/mzn_optimization_test.rs
+++ pumpkin-solver/tests/mzn_optimization_test.rs
@@ -3,12 +3,15 @@
 mod helpers;
 
 use helpers::run_mzn_test;
+use helpers::TestType;
 
 macro_rules! mzn_optimization_test {
     ($name:ident) => {
         #[test]
         fn $name() {
-            run_mzn_test::<false>(stringify!($name), "mzn_optimization");
+            let output =
+                run_mzn_test::<false>(stringify!($name), "mzn_optimization", TestType::Optimality);
+            assert!(output.ends_with("==========\n"));
         }
     };
 }
