d9eb7b60b393be42cb60986c2ae75084540b69c4
Emir Demirović <edemirovic@tudelft.nl>
2025-04-24 15:04:17 +0200

    renamed trailed_assignments file into trailed_values

--- pumpkin-solver/src/engine/cp/trailed/mod.rs
+++ pumpkin-solver/src/engine/cp/trailed/mod.rs
@@ -1,6 +1,6 @@
-mod trailed_assignments;
 mod trailed_change;
 mod trailed_integer;
-pub(crate) use trailed_assignments::TrailedValues;
+mod trailed_values;
 pub(crate) use trailed_change::TrailedChange;
 pub(crate) use trailed_integer::TrailedInteger;
+pub(crate) use trailed_values::TrailedValues;
