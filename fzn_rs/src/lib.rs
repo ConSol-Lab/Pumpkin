6bfb4bb1cfd4b03666cecc556991751654315b3e
Maarten Flippo <maartenflippo@outlook.com>
2025-07-04 12:08:20 +0200

    docs(fzn-rs): Add root module documentation to fzn-rs

--- fzn_rs/src/lib.rs
+++ fzn_rs/src/lib.rs
@@ -1,3 +1,45 @@
+//! # fzn-rs
+//!
+//! `fzn-rs` is a crate that allows for easy parsing of FlatZinc instances in Rust.
+//!
+//! ## Comparison to other FlatZinc crates
+//! There are two well-known crates for parsing FlatZinc files:
+//! - [flatzinc](https://docs.rs/flatzinc), for parsing the original `fzn` format,
+//! - and [flatzinc-serde](https://docs.rs/flatzinc-serde), for parsing `fzn.json`.
+//!
+//! The goal of this crate is to be able to parse both the original `fzn` format, as well as the
+//! newer `fzn.json` format. Additionally, there is a derive macro that allows for strongly-typed
+//! constraints as they are supported by your application. Finally, our aim is to improve the error
+//! messages that are encountered when parsing invalid FlatZinc files.
+//!
+//! ## Derive Macro
+//! When parsing a FlatZinc file, the result is an [`ast::Ast`]. That type describes any valid
+//! FlatZinc file. However, when consuming FlatZinc, typically you need to process that AST
+//! further. For example, to support the [`int_lin_le`][1] constraint, you have to validate that the
+//! [`ast::Constraint`] has three arguments, and that each of the arguments has the correct type.
+//!
+//! When using this crate with the `derive` feature, you can instead do the following:
+//! ```rust
+//! #[derive(FlatZincConstraint)]
+//! pub enum MyConstraints {
+//!     /// The variant name is converted to snake_case to serve as the constraint identifier by
+//!     /// default.
+//!     IntLinLe(Vec<i64>, Vec<IntVariable>, i64),
+//!
+//!     /// If the snake_case version of the variant name is different from the constraint
+//!     /// identifier, then the `#[name(...)], attribute allows you to set the constraint
+//!     /// identifier explicitly.
+//!     #[name("int_lin_eq")]
+//!     LinearEquality(Vec<i64>, Vec<IntVariable>, i64),
+//! }
+//! ```
+//! The macro automatically implements [`from_ast::FlatZincConstraint`] and will handle the parsing
+//! of arguments for you.
+//!
+//! [1]: https://docs.minizinc.dev/en/stable/lib-flatzinc-int.html#int-lin-le
+#[cfg(feature = "derive")]
+pub use fzn_rs_derive::*;
+
 pub mod ast;
 
 mod error;
