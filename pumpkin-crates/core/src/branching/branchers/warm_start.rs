6de9d1f4feeefe338e9ecbe3859a1c4660c598f5
Imko Marijnissen <imko.marijnissen@gmail.com>
2025-07-06 12:01:27 +0200

    refactor: changing warm start to keep attempting to reassign until a solution is found

--- pumpkin-crates/core/src/branching/branchers/warm_start.rs
+++ pumpkin-crates/core/src/branching/branchers/warm_start.rs
@@ -1,21 +1,25 @@
 use crate::branching::Brancher;
 use crate::branching::BrancherEvent;
 use crate::branching::SelectionContext;
+use crate::engine::Assignments;
 use crate::predicate;
 use crate::predicates::Predicate;
 use crate::pumpkin_assert_eq_simple;
+use crate::results::SolutionReference;
 use crate::variables::IntegerVariable;
 
 /// A [`Brancher`] which represents warm starting.
 ///
-/// It takes a list of variables and values (of equal size) and attempts to assign each variable to
-/// the value *once* if possible. If it is not possible, then this [`Brancher`] will not attempt to
-/// assign that variable to that value again (unless it is specified multiple times in the warm
-/// start).
+/// It takes a list of variables and values (of equal size) and attempts to assign each variable
+/// `variables[i]` to the value `values[i]`. If this is not possible, then it will attempt to assign
+/// the next variable and so on, until it either runs out of variables or a solution is found.
 #[derive(Debug)]
 pub struct WarmStart<Var> {
     variables: Vec<Var>,
     values: Vec<i32>,
+
+    has_found_solution: bool,
+    index: usize,
 }
 
 impl<Var: Clone> WarmStart<Var> {
@@ -24,15 +28,26 @@ impl<Var: Clone> WarmStart<Var> {
         Self {
             variables: variables.iter().rev().cloned().collect(),
             values: values.iter().rev().cloned().collect(),
+
+            has_found_solution: false,
+            index: 0,
         }
     }
 }
 
 impl<Var: IntegerVariable> Brancher for WarmStart<Var> {
     fn next_decision(&mut self, context: &mut SelectionContext) -> Option<Predicate> {
-        pumpkin_assert_eq_simple!(self.variables.len(), self.values.len());
-        while let (Some(variable), Some(value)) = (self.variables.pop(), self.values.pop()) {
+        if self.has_found_solution {
+            return None;
+        }
+
+        while self.index < self.variables.len() {
+            let variable = &self.variables[self.index];
+            let value = self.values[self.index];
+
             let predicate = predicate!(variable == value);
+
+            self.index += 1;
 
             if context.assignments.evaluate_predicate(predicate).is_none() {
                 return Some(predicate);
@@ -42,7 +57,15 @@ impl<Var: IntegerVariable> Brancher for WarmStart<Var> {
         None
     }
 
+    fn synchronise(&mut self, _assignments: &Assignments) {
+        self.index = 0
+    }
+
+    fn on_solution(&mut self, _solution: SolutionReference) {
+        self.has_found_solution = true;
+    }
+
     fn subscribe_to_events(&self) -> Vec<BrancherEvent> {
-        vec![]
+        vec![BrancherEvent::Solution, BrancherEvent::Synchronise]
     }
 }
