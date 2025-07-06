a922bae98355fc22b36651dc50a8b2b7e51e4ed8
Maarten Flippo <maartenflippo@outlook.com>
2025-07-06 09:24:44 +0200

    feat(fzn-rs): Implement array annotation arguments

--- fzn_rs/src/from_ast.rs
+++ fzn_rs/src/from_ast.rs
@@ -31,7 +31,21 @@ pub trait FlatZincConstraint: Sized {
 /// [`FlatZincAnnotation::from_ast`] can successfully parse an annotation into nothing, signifying
 /// the annotation is not of interest in the final [`crate::Instance`].
 pub trait FlatZincAnnotation: Sized {
+    /// Parse a value of `Self` from the annotation node. Return `None` if the annotation node
+    /// clearly is not relevant for `Self`, e.g. when the name is for a completely different
+    /// annotation than `Self` models.
     fn from_ast(annotation: &ast::Annotation) -> Result<Option<Self>, InstanceError>;
+
+    /// Parse an [`ast::Annotation`] into `Self` and produce an error if the annotation cannot be
+    /// converted to a value of `Self`.
+    fn from_ast_required(annotation: &ast::Annotation) -> Result<Self, InstanceError> {
+        let outcome = Self::from_ast(annotation)?;
+
+        // By default, failing to parse an annotation node into an annotation type is not
+        // necessarily an error since the annotation node can be ignored. In this case, however,
+        // we require a value to be present. Hence, if `outcome` is `None`, that is an error.
+        outcome.ok_or_else(|| InstanceError::UnsupportedAnnotation(annotation.name().into()))
+    }
 }
 
 /// A default implementation that ignores all annotations.
@@ -200,10 +214,14 @@ impl<T: FromLiteral> FromArgument for Vec<T> {
     }
 }
 
-pub trait FromAnnotationArgument<Out = Self>: Sized {
-    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Out, InstanceError>;
+/// Parse a value from an [`ast::AnnotationArgument`].
+///
+/// Any type that implements [`FromAnnotationLiteral`] also implements [`FromAnnotationArgument`].
+pub trait FromAnnotationArgument: Sized {
+    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Self, InstanceError>;
 }
 
+/// Parse a value from an [`ast::AnnotationLiteral`].
 pub trait FromAnnotationLiteral: Sized {
     fn expected() -> Token;
 
@@ -244,36 +262,81 @@ impl<T: FromAnnotationLiteral> FromAnnotationArgument for T {
     }
 }
 
-/// Parse a nested annotation from an annotation argument.
-pub fn from_nested_annotation<T: FlatZincAnnotation>(
-    argument: &ast::Node<ast::AnnotationArgument>,
-) -> Result<T, InstanceError> {
-    let annotation = match &argument.node {
-        ast::AnnotationArgument::Literal(literal) => match &literal.node {
-            ast::AnnotationLiteral::BaseLiteral(ast::Literal::Identifier(ident)) => {
-                ast::Annotation::Atom(Rc::clone(ident))
-            }
-            ast::AnnotationLiteral::Annotation(annotation_call) => {
-                ast::Annotation::Call(annotation_call.clone())
-            }
-            ast::AnnotationLiteral::BaseLiteral(lit) => {
+impl<T: FromAnnotationLiteral> FromAnnotationArgument for Vec<T> {
+    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Self, InstanceError> {
+        match &argument.node {
+            ast::AnnotationArgument::Array(array) => array
+                .iter()
+                .map(|literal| T::from_literal(literal))
+                .collect(),
+
+            node => Err(InstanceError::UnexpectedToken {
+                expected: Token::Array,
+                actual: node.into(),
+                span: argument.span,
+            }),
+        }
+    }
+}
+
+/// Parse an [`ast::AnnotationArgument`] as an annotation. This needs to be a separate trait from
+/// [`FromAnnotationArgument`] so it does not collide wiith implementations for literals.
+pub trait FromNestedAnnotation: Sized {
+    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Self, InstanceError>;
+}
+
+/// Converts an [`ast::AnnotationLiteral`] to an [`ast::Annotation`], or produces an error if that
+/// is not possible.
+fn annotation_literal_to_annotation(
+    literal: &ast::Node<ast::AnnotationLiteral>,
+) -> Result<ast::Annotation, InstanceError> {
+    match &literal.node {
+        ast::AnnotationLiteral::BaseLiteral(ast::Literal::Identifier(ident)) => {
+            Ok(ast::Annotation::Atom(Rc::clone(ident)))
+        }
+        ast::AnnotationLiteral::Annotation(annotation_call) => {
+            Ok(ast::Annotation::Call(annotation_call.clone()))
+        }
+        ast::AnnotationLiteral::BaseLiteral(lit) => Err(InstanceError::UnexpectedToken {
+            expected: Token::Annotation,
+            actual: lit.into(),
+            span: literal.span,
+        }),
+    }
+}
+
+impl<Ann: FlatZincAnnotation> FromNestedAnnotation for Ann {
+    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Ann, InstanceError> {
+        let annotation = match &argument.node {
+            ast::AnnotationArgument::Literal(literal) => annotation_literal_to_annotation(literal)?,
+            ast::AnnotationArgument::Array(_) => {
                 return Err(InstanceError::UnexpectedToken {
                     expected: Token::Annotation,
-                    actual: lit.into(),
-                    span: literal.span,
+                    actual: Token::Array,
+                    span: argument.span,
                 });
             }
-        },
-        ast::AnnotationArgument::Array(_) => {
-            return Err(InstanceError::UnexpectedToken {
-                expected: Token::Annotation,
-                actual: Token::Array,
+        };
+
+        Ann::from_ast_required(&annotation)
+    }
+}
+
+impl<Ann: FlatZincAnnotation> FromNestedAnnotation for Vec<Ann> {
+    fn from_argument(argument: &ast::Node<ast::AnnotationArgument>) -> Result<Self, InstanceError> {
+        match &argument.node {
+            ast::AnnotationArgument::Array(elements) => elements
+                .iter()
+                .map(|literal| {
+                    let annotation = annotation_literal_to_annotation(literal)?;
+                    Ann::from_ast_required(&annotation)
+                })
+                .collect::<Result<_, _>>(),
+            ast::AnnotationArgument::Literal(lit) => Err(InstanceError::UnexpectedToken {
+                expected: Token::Array,
+                actual: (&lit.node).into(),
                 span: argument.span,
-            });
+            }),
         }
-    };
-
-    let outcome = T::from_ast(&annotation)?;
-
-    outcome.ok_or_else(|| InstanceError::UnsupportedAnnotation(annotation.name().into()))
+    }
 }
