e13302fd3a7e7b6a313a4ad9717779af5f6bb3d9
Maarten Flippo <maartenflippo@outlook.com>
2025-07-25 13:47:52 +0200

    refactor(fzn-rs): Clean up the implementation into modules + documentation

---
+++ fzn-rs/src/typed/flatzinc_constraint.rs
@@ -0,0 +1,28 @@
+use crate::ast;
+use crate::InstanceError;
+use crate::Token;
+
+use super::FromLiteral;
+
+/// Parse a constraint from the given [`ast::Constraint`].
+pub trait FlatZincConstraint: Sized {
+    fn from_ast(constraint: &ast::Constraint) -> Result<Self, InstanceError>;
+}
+
+/// Extract an argument from the [`ast::Argument`] node.
+pub trait FromArgument: Sized {
+    fn from_argument(argument: &ast::Node<ast::Argument>) -> Result<Self, InstanceError>;
+}
+
+impl<T: FromLiteral> FromArgument for T {
+    fn from_argument(argument: &ast::Node<ast::Argument>) -> Result<Self, InstanceError> {
+        match &argument.node {
+            ast::Argument::Literal(literal) => T::from_literal(literal),
+            ast::Argument::Array(_) => Err(InstanceError::UnexpectedToken {
+                expected: T::expected(),
+                actual: Token::Array,
+                span: argument.span,
+            }),
+        }
+    }
+}
