ceebe9b145cb645b8bbe81b4b76b7727575221ab
Imko Marijnissen <imko.marijnissen@gmail.com>
2025-09-12 10:06:03 +0900

    feat: adding unbounded integers

--- pumpkin-solver/src/bin/pumpkin-solver/flatzinc/parser.rs
+++ pumpkin-solver/src/bin/pumpkin-solver/flatzinc/parser.rs
@@ -9,6 +9,15 @@ use super::ast::SingleVarDecl;
 use super::ast::VarArrayDecl;
 use super::FlatZincError;
 
+/// The minimum value which an integer variable can take on.
+///
+/// It is divided by 2 to avoid underflows.
+const MIN_VALUE: i32 = i32::MIN / 2;
+/// The maximum value which an integer variable can take on.
+///
+/// It is divided by 2 to avoid overflows.
+const MAX_VALUE: i32 = i32::MAX / 2;
+
 pub(crate) fn parse(source: impl Read) -> Result<FlatZincAst, FlatZincError> {
     let reader = BufReader::new(source);
 
@@ -131,8 +140,16 @@ fn parse_var_decl(
             Ok(())
         }
 
-        flatzinc::VarDeclItem::Int { .. } => {
-            Err(FlatZincError::UnsupportedVariable("unbounded int".into()))
+        flatzinc::VarDeclItem::Int { id, expr, annos } => {
+            // For unbounded integers, we take the minimum and maximum possible values
+            ast.add_variable_decl(SingleVarDecl::IntInRange {
+                id,
+                lb: MIN_VALUE as i128,
+                ub: MAX_VALUE as i128,
+                expr,
+                annos,
+            });
+            Ok(())
         }
 
         flatzinc::VarDeclItem::Float { .. }
