6b1e78e6db09b4d2d431e38458d9e5654e9ec7ee
Maarten Flippo <maartenflippo@outlook.com>
2025-11-12 16:07:48 +0100

    feat(pumpkin-solver-py): Add incrementality to python interface
    We have to annotate `Model` as `unsendable`, which means the python
    script will crash if the interpreter moves the object between threads.
    It remains to be seen whether this happens in practice, and if so, we
    will need to engineer our way around it.

--- pumpkin-solver-py/src/variables.rs
+++ pumpkin-solver-py/src/variables.rs
@@ -1,86 +1,26 @@
-use pumpkin_solver::containers::KeyedVec;
-use pumpkin_solver::containers::StorageKey;
 use pumpkin_solver::predicate;
 use pumpkin_solver::variables::AffineView;
 use pumpkin_solver::variables::DomainId;
 use pumpkin_solver::variables::Literal;
-use pumpkin_solver::variables::TransformableVariable;
 use pyo3::prelude::*;
 
-#[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
-pub struct IntVariable(usize);
+#[pyclass(eq, hash, frozen)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct IntExpression(pub AffineView<DomainId>);
 
-impl StorageKey for IntVariable {
-    fn index(&self) -> usize {
-        self.0
-    }
-
-    fn create_from_index(index: usize) -> Self {
-        IntVariable(index)
+impl From<DomainId> for IntExpression {
+    fn from(domain_id: DomainId) -> IntExpression {
+        IntExpression(domain_id.into())
     }
 }
 
 #[pyclass(eq, hash, frozen)]
-#[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
-pub struct IntExpression {
-    pub variable: IntVariable,
-    pub offset: i32,
-    pub scale: i32,
-}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct BoolExpression(pub Literal);
 
-impl From<IntVariable> for IntExpression {
-    fn from(variable: IntVariable) -> Self {
-        IntExpression {
-            variable,
-            offset: 0,
-            scale: 1,
-        }
-    }
-}
-
-impl IntExpression {
-    pub fn to_affine_view(self, variable_map: &VariableMap) -> AffineView<DomainId> {
-        let IntExpression {
-            variable,
-            offset,
-            scale,
-        } = self;
-
-        variable_map
-            .get_integer(variable)
-            .scaled(scale)
-            .offset(offset)
-    }
-}
-
-#[pymethods]
-impl IntExpression {
-    fn offset(&self, add_offset: i32) -> IntExpression {
-        let IntExpression {
-            variable,
-            offset,
-            scale,
-        } = *self;
-
-        IntExpression {
-            variable,
-            offset: offset + add_offset,
-            scale,
-        }
-    }
-
-    fn scaled(&self, scaling: i32) -> IntExpression {
-        let IntExpression {
-            variable,
-            offset,
-            scale,
-        } = *self;
-
-        IntExpression {
-            variable,
-            offset: offset * scaling,
-            scale: scale * scaling,
-        }
+impl From<Literal> for BoolExpression {
+    fn from(literal: Literal) -> BoolExpression {
+        BoolExpression(literal)
     }
 }
 
@@ -114,12 +54,8 @@ impl Predicate {
 }
 
 impl Predicate {
-    /// Convert the predicate in the model domain to a predicate in the solver domain.
-    pub(crate) fn to_solver_predicate(
-        self,
-        variable_map: &VariableMap,
-    ) -> pumpkin_solver::predicates::Predicate {
-        let affine_view = self.variable.to_affine_view(variable_map);
+    pub(crate) fn into_solver_predicate(self) -> pumpkin_solver::predicates::Predicate {
+        let affine_view = self.variable.0;
 
         match self.comparator {
             Comparator::NotEqual => predicate![affine_view != self.value],
@@ -127,67 +63,5 @@ impl Predicate {
             Comparator::LessThanOrEqual => predicate![affine_view <= self.value],
             Comparator::GreaterThanOrEqual => predicate![affine_view >= self.value],
         }
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq)]
-pub struct BoolVariable(usize);
-
-impl StorageKey for BoolVariable {
-    fn index(&self) -> usize {
-        self.0
-    }
-
-    fn create_from_index(index: usize) -> Self {
-        BoolVariable(index)
-    }
-}
-
-#[pyclass(eq)]
-#[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq)]
-pub struct BoolExpression(BoolVariable, bool);
-
-impl From<BoolVariable> for BoolExpression {
-    fn from(value: BoolVariable) -> Self {
-        BoolExpression(value, true)
-    }
-}
-
-impl BoolExpression {
-    pub fn get_polarity(self) -> bool {
-        self.1
-    }
-
-    pub fn get_variable(self) -> BoolVariable {
-        self.0
-    }
-
-    pub fn to_literal(self, variable_map: &VariableMap) -> Literal {
-        let literal = variable_map.get_boolean(self.0);
-
-        if self.1 { literal } else { !literal }
-    }
-}
-
-#[pymethods]
-impl BoolExpression {
-    pub fn negate(&self) -> Self {
-        BoolExpression(self.0, !self.1)
-    }
-}
-
-#[derive(Clone, Default)]
-pub struct VariableMap {
-    pub integers: KeyedVec<IntVariable, AffineView<DomainId>>,
-    pub booleans: KeyedVec<BoolVariable, Literal>,
-}
-
-impl VariableMap {
-    pub fn get_integer(&self, variable: IntVariable) -> AffineView<DomainId> {
-        self.integers[variable]
-    }
-
-    pub fn get_boolean(&self, variable: BoolVariable) -> Literal {
-        self.booleans[variable]
     }
 }
