05ad4a2ace5cc5e07edcd39f304976417c9d5e0a
Maarten Flippo <maartenflippo@outlook.com>
2025-11-12 16:52:45 +0100

    fix: Add new variables to the brancher

--- pumpkin-crates/core/src/branching/branchers/autonomous_search.rs
+++ pumpkin-crates/core/src/branching/branchers/autonomous_search.rs
@@ -135,6 +135,10 @@ impl DefaultBrancher {
             statistics: Default::default(),
         }
     }
+
+    pub fn add_domain(&mut self, domain: DomainId) {
+        self.backup_brancher.variable_selector.add_domain(domain);
+    }
 }
 
 impl<BackupSelector> AutonomousSearch<BackupSelector> {
