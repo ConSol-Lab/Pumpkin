e1ab35bed24ad973eb5e6bdacbb7a3ab23b6eab1
Imko Marijnissen <imko.marijnissen@gmail.com>
2025-11-18 16:31:48 +0100

    chore: remove useless trait implementation

--- pumpkin-crates/core/src/basic_types/propagation_status_cp.rs
+++ pumpkin-crates/core/src/basic_types/propagation_status_cp.rs
@@ -1,6 +1,4 @@
 use super::PropositionalConjunction;
-#[cfg(test)]
-use crate::basic_types::StoredConflictInfo;
 use crate::engine::EmptyDomain;
 use crate::proof::InferenceCode;
 use crate::state::Conflict;
@@ -36,19 +34,6 @@ impl From<EmptyDomain> for Inconsistency {
 impl From<PropagatorConflict> for Inconsistency {
     fn from(conflict: PropagatorConflict) -> Self {
         Inconsistency::Conflict(conflict)
-    }
-}
-
-#[cfg(test)]
-impl TryFrom<StoredConflictInfo> for Inconsistency {
-    type Error = &'static str;
-
-    fn try_from(value: StoredConflictInfo) -> Result<Self, Self::Error> {
-        match value {
-            StoredConflictInfo::Propagator(propagator_conflict) => Ok(propagator_conflict.into()),
-            StoredConflictInfo::EmptyDomain(_) => Ok(Inconsistency::EmptyDomain),
-            _ => Err("Could not convert"),
-        }
     }
 }
 
