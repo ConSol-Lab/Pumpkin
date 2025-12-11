a09a2a7ab115d27bfcaa59d8d7410a44ce114a0d
Maarten Flippo <maartenflippo@outlook.com>
2025-12-11 12:13:39 +0100

    refactor(pumpkin-checker): Document all-different checker

--- pumpkin-checker/src/inferences/all_different.rs
+++ pumpkin-checker/src/inferences/all_different.rs
@@ -15,6 +15,10 @@ pub(crate) fn verify_all_different(
     fact: &Fact,
     constraint: &Constraint,
 ) -> Result<(), InvalidInference> {
+    // This checker takes the union of the domains of the variables in the constraint. If there
+    // are fewer values in the union of the domain than there are variables, then there is a
+    // conflict and the inference is valid.
+
     let Constraint::AllDifferent(all_different) = constraint else {
         return Err(InvalidInference::ConstraintLabelMismatch);
     };
@@ -22,6 +26,7 @@ pub(crate) fn verify_all_different(
     let variable_state = VariableState::prepare_for_conflict_check(fact)
         .ok_or(InvalidInference::InconsistentPremises)?;
 
+    // Collect all values present in at least one of the domains.
     let union_of_domains = all_different
         .variables
         .iter()
@@ -29,6 +34,9 @@ pub(crate) fn verify_all_different(
         .flatten()
         .collect::<HashSet<_>>();
 
+    // Collect the variables mentioned in the fact. Here we ignore variables with a domain
+    // equal to all integers, as they are not mentioned in the fact. Therefore they do not
+    // contribute in the hall-set reasoning.
     let variables = all_different
         .variables
         .iter()
