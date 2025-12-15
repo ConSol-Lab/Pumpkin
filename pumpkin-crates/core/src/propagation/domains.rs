062054ace3b021f0f4569b2e30fba381be0cb87f
Imko Marijnissen <imko.marijnissen@gmail.com>
2025-12-15 11:35:30 +0100

    docs: adding documentation to ReadDomains

--- pumpkin-crates/core/src/propagation/domains.rs
+++ pumpkin-crates/core/src/propagation/domains.rs
@@ -2,6 +2,8 @@ use crate::engine::Assignments;
 use crate::engine::TrailedInteger;
 use crate::engine::TrailedValues;
 use crate::predicates::Predicate;
+#[cfg(doc)]
+use crate::propagation::ExplanationContext;
 use crate::variables::IntegerVariable;
 use crate::variables::Literal;
 
@@ -40,14 +42,19 @@ impl HasAssignments for Domains<'_> {
     }
 }
 
+/// A trait defining functions for retrieving information about the current domains.
 pub trait ReadDomains {
+    /// Returns whether the provided [`Predicate`] is assigned (either true or false) or is
+    /// currently unassigned.
     fn evaluate_predicate(&self, predicate: Predicate) -> Option<bool>;
 
+    /// Returns whether the provided [`Literal`] is assigned (either true or false) or is
+    /// currently unassigned.
     fn evaluate_literal(&self, literal: Literal) -> Option<bool> {
         self.evaluate_predicate(literal.get_true_predicate())
     }
 
-    /// Returns the holes which were created on the current decision level.
+    /// Returns the holes in the domain which were created on the current checkpoint.
     fn get_holes_at_current_checkpoint<Var: IntegerVariable>(
         &self,
         var: &Var,
@@ -57,27 +64,44 @@ pub trait ReadDomains {
     /// created at previous decision levels).
     fn get_holes<Var: IntegerVariable>(&self, var: &Var) -> impl Iterator<Item = i32>;
 
-    /// Returns `true` if the domain of the given variable is singleton.
+    /// Returns `true` if the domain of the given variable is singleton (i.e., the variable is
+    /// fixed).
     fn is_fixed<Var: IntegerVariable>(&self, var: &Var) -> bool;
 
+    /// Returns the lowest value in the domain of `var`.
     fn lower_bound<Var: IntegerVariable>(&self, var: &Var) -> i32;
 
+    /// Returns the lowest value in the domain of `var` at the given `trail_position`.
+    ///
+    /// The trail position can be retrieved when generating lazy explanations using
+    /// [`ExplanationContext::get_trail_position`].
     fn lower_bound_at_trail_position<Var: IntegerVariable>(
         &self,
         var: &Var,
         trail_position: usize,
     ) -> i32;
 
+    /// Returns the highest value in the domain of `var`.
     fn upper_bound<Var: IntegerVariable>(&self, var: &Var) -> i32;
 
+    /// Returns the highest value in the domain of `var` at the given `trail_position`.
+    ///
+    /// The trail position can be retrieved when generating lazy explanations using
+    /// [`ExplanationContext::get_trail_position`].
     fn upper_bound_at_trail_position<Var: IntegerVariable>(
         &self,
         var: &Var,
         trail_position: usize,
     ) -> i32;
 
+    /// Returns whether the provided `value` is in the domain of `var`.
     fn contains<Var: IntegerVariable>(&self, var: &Var, value: i32) -> bool;
 
+    /// Returns whether the provided `value` is in the domain of `var` at the given
+    /// `trail_position`.
+    ///
+    /// The trail position can be retrieved when generating lazy explanations using
+    /// [`ExplanationContext::get_trail_position`].
     fn contains_at_trail_position<Var: IntegerVariable>(
         &self,
         var: &Var,
@@ -85,10 +109,16 @@ pub trait ReadDomains {
         trail_position: usize,
     ) -> bool;
 
+    /// Returns an [`Iterator`] over the values in the domain of the provided `var` (including the
+    /// lower-bound and upper-bound values).
     fn iterate_domain<Var: IntegerVariable>(&self, var: &Var) -> impl Iterator<Item = i32>;
 
+    /// Returns whether the provided [`Predicate`] was posted as a decision (i.e., it was posted as
+    /// a [`Predicate`] without a reason).
     fn is_decision_predicate(&self, predicate: Predicate) -> bool;
 
+    /// If the provided [`Predicate`] is true, then this method returns the checkpoint at which it
+    /// first become true; otherwise, it returns [`None`].
     fn get_checkpoint_for_predicate(&self, predicate: Predicate) -> Option<usize>;
 }
 
@@ -97,7 +127,6 @@ impl<T: HasAssignments> ReadDomains for T {
         self.assignments().evaluate_predicate(predicate)
     }
 
-    /// Returns the holes which were created on the current decision level.
     fn get_holes_at_current_checkpoint<Var: IntegerVariable>(
         &self,
         var: &Var,
@@ -105,13 +134,10 @@ impl<T: HasAssignments> ReadDomains for T {
         var.get_holes_at_current_checkpoint(self.assignments())
     }
 
-    /// Returns all of the holes (currently) in the domain of `var` (including ones which were
-    /// created at previous decision levels).
     fn get_holes<Var: IntegerVariable>(&self, var: &Var) -> impl Iterator<Item = i32> {
         var.get_holes(self.assignments())
     }
 
-    /// Returns `true` if the domain of the given variable is singleton.
     fn is_fixed<Var: IntegerVariable>(&self, var: &Var) -> bool {
         self.lower_bound(var) == self.upper_bound(var)
     }
@@ -157,14 +183,10 @@ impl<T: HasAssignments> ReadDomains for T {
         var.iterate_domain(self.assignments())
     }
 
-    /// Returns whether the provided [`Predicate`] was posted as a decision (i.e., it was posted as
-    /// a [`Predicate`] without a reason).
     fn is_decision_predicate(&self, predicate: Predicate) -> bool {
         self.assignments().is_decision_predicate(&predicate)
     }
 
-    /// If the provided [`Predicate`] is true, then this method returns the checkpoint at which it
-    /// first become true; otherwise, it returns [`None`].
     fn get_checkpoint_for_predicate(&self, predicate: Predicate) -> Option<usize> {
         self.assignments().get_checkpoint_for_predicate(&predicate)
     }
