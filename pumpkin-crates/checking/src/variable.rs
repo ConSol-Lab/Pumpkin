cd9e2081b8fafc4f2e4a88205bb122662629c2f7
Maarten Flippo <maartenflippo@outlook.com>
2026-01-11 13:52:37 +0100

    Update documentation for pumpkin-checking

---
+++ pumpkin-crates/checking/src/variable.rs
@@ -0,0 +1,45 @@
+use std::fmt::Debug;
+
+use crate::{AtomicConstraint, I32Ext, VariableState};
+
+/// A variable in a constraint satisfaction problem.
+pub trait CheckerVariable<Atomic: AtomicConstraint>: Debug + Clone {
+    /// Get the atomic constraint `[self <= value]`.
+    fn atomic_less_than(&self, value: i32) -> Atomic;
+
+    /// Get the atomic constraint `[self <= value]`.
+    fn atomic_greater_than(&self, value: i32) -> Atomic;
+
+    /// Get the atomic constraint `[self == value]`.
+    fn atomic_equal(&self, value: i32) -> Atomic;
+
+    /// Get the atomic constraint `[self != value]`.
+    fn atomic_not_equal(&self, value: i32) -> Atomic;
+
+    /// Get the lower bound of the domain.
+    fn induced_lower_bound(&self, variable_state: &VariableState<Atomic>) -> I32Ext;
+
+    /// Get the upper bound of the domain.
+    fn induced_upper_bound(&self, variable_state: &VariableState<Atomic>) -> I32Ext;
+
+    /// Get the value the variable is fixed to, if the variable is fixed.
+    fn induced_fixed_value(&self, variable_state: &VariableState<Atomic>) -> Option<i32>;
+
+    /// Get the holes in the domain.
+    fn induced_holes<'this, 'state>(
+        &'this self,
+        variable_state: &'state VariableState<Atomic>,
+    ) -> impl Iterator<Item = i32> + 'state
+    where
+        'this: 'state;
+
+    /// Iterate the domain of the variable.
+    ///
+    /// The order of the values is unspecified.
+    fn iter_induced_domain<'this, 'state>(
+        &'this self,
+        variable_state: &'state VariableState<Atomic>,
+    ) -> Option<impl Iterator<Item = i32> + 'state>
+    where
+        'this: 'state;
+}
