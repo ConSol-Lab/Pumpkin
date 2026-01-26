5473e62efb2cce76d3a92a22156ed466595af2c0
Maarten Flippo <maartenflippo@outlook.com>
2026-01-26 15:00:06 +0100

    Normalize linear inequalities

--- pumpkin-crates/core/src/propagators/hypercube_linear/linear.rs
+++ pumpkin-crates/core/src/propagators/hypercube_linear/linear.rs
@@ -1,6 +1,9 @@
 use std::num::NonZero;
 
-use crate::variables::{AffineView, DomainId, TransformableVariable};
+use crate::containers::HashMap;
+use crate::variables::AffineView;
+use crate::variables::DomainId;
+use crate::variables::TransformableVariable;
 
 /// The linear inequality part of a hypercube linear constraint.
 #[derive(Clone, Debug)]
@@ -10,12 +13,123 @@ pub struct LinearInequality {
 }
 
 impl LinearInequality {
-    pub fn new(terms: impl IntoIterator<Item = (NonZero<i32>, DomainId)>, bound: i32) -> Self {
-        let terms = terms
-            .into_iter()
-            .map(|(weight, domain)| domain.scaled(weight.get()))
-            .collect();
+    /// Construct a new linear inequality.
+    ///
+    /// If the terms simplify to 0 and the `bound` is less than 0, then `None` is returned.
+    pub fn new(
+        terms: impl IntoIterator<Item = (NonZero<i32>, DomainId)>,
+        bound: i32,
+    ) -> Option<Self> {
+        // To merge terms with the same domain, we go through a HashMap mapping the weight to the
+        // domain.
+        let mut domain_to_weight = HashMap::new();
 
-        LinearInequality { terms, bound }
+        for (weight, domain_id) in terms {
+            let existing_weight = domain_to_weight.entry(domain_id).or_insert(0);
+            *existing_weight += weight.get();
+        }
+
+        let terms = domain_to_weight
+            .into_iter()
+            .filter(|&(_, weight)| weight != 0)
+            .map(|(domain, weight)| domain.scaled(weight))
+            .collect::<Box<[_]>>();
+
+        if terms.is_empty() && bound < 0 {
+            return None;
+        }
+
+        Some(LinearInequality { terms, bound })
+    }
+
+    /// Iterate over the terms in the linear inequality.
+    pub fn terms(&self) -> impl Iterator<Item = AffineView<DomainId>> + '_ {
+        self.terms.iter().copied()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{containers::HashSet, state::State};
+
+    use super::*;
+
+    #[test]
+    fn terms_are_iterable() {
+        let mut state = State::default();
+
+        let x = state.new_interval_variable(1, 10, Some("x".into()));
+        let y = state.new_interval_variable(1, 10, Some("y".into()));
+
+        let linear = LinearInequality::new(
+            [(NonZero::new(2).unwrap(), x), (NonZero::new(3).unwrap(), y)],
+            8,
+        )
+        .expect("not trivially false");
+
+        let iterated_terms = linear.terms().collect::<HashSet<_>>();
+
+        assert_eq!(
+            [x.scaled(2), y.scaled(3)]
+                .into_iter()
+                .collect::<HashSet<_>>(),
+            iterated_terms
+        );
+    }
+
+    #[test]
+    fn terms_for_same_variable_are_merged() {
+        let mut state = State::default();
+
+        let x = state.new_interval_variable(1, 10, Some("x".into()));
+
+        let linear = LinearInequality::new(
+            [(NonZero::new(2).unwrap(), x), (NonZero::new(3).unwrap(), x)],
+            8,
+        )
+        .expect("not trivially false");
+
+        let iterated_terms = linear.terms().collect::<HashSet<_>>();
+
+        assert_eq!(
+            [x.scaled(5)].into_iter().collect::<HashSet<_>>(),
+            iterated_terms
+        );
+    }
+
+    #[test]
+    fn trivially_satisfied_linear_inequalities_are_okay() {
+        let mut state = State::default();
+
+        let x = state.new_interval_variable(1, 10, Some("x".into()));
+
+        let linear = LinearInequality::new(
+            [
+                (NonZero::new(2).unwrap(), x),
+                (NonZero::new(-2).unwrap(), x),
+            ],
+            8,
+        )
+        .expect("not trivially false");
+
+        let iterated_terms = linear.terms().collect::<HashSet<_>>();
+        assert_eq!(HashSet::<AffineView<DomainId>>::default(), iterated_terms);
+    }
+
+    #[test]
+    fn trivially_unsatisfied_linear_cannot_be_created() {
+        let mut state = State::default();
+
+        let x = state.new_interval_variable(1, 10, Some("x".into()));
+
+        let linear = LinearInequality::new(
+            [
+                (NonZero::new(2).unwrap(), x),
+                (NonZero::new(-2).unwrap(), x),
+            ],
+            -1,
+        );
+
+        assert!(linear.is_none());
     }
 }
--- pumpkin-crates/core/src/propagators/hypercube_linear/mod.rs
+++ pumpkin-crates/core/src/propagators/hypercube_linear/mod.rs
@@ -1,18 +1,20 @@
 mod hypercube;
 mod linear;
 
-use crate::{
-    basic_types::PredicateId,
-    proof::ConstraintTag,
-    propagation::{
-        EnqueueDecision, LocalId, NotificationContext, OpaqueDomainEvent, PropagationContext,
-        Propagator, PropagatorConstructor, PropagatorConstructorContext,
-    },
-    results::PropagationStatusCP,
-};
-
 pub use hypercube::*;
 pub use linear::*;
+
+use crate::basic_types::PredicateId;
+use crate::proof::ConstraintTag;
+use crate::propagation::EnqueueDecision;
+use crate::propagation::LocalId;
+use crate::propagation::NotificationContext;
+use crate::propagation::OpaqueDomainEvent;
+use crate::propagation::PropagationContext;
+use crate::propagation::Propagator;
+use crate::propagation::PropagatorConstructor;
+use crate::propagation::PropagatorConstructorContext;
+use crate::results::PropagationStatusCP;
 
 /// The [`PropagatorConstructor`] for the [`HypercubeLinearPropagator`].
 #[derive(Clone, Debug)]
@@ -25,8 +27,23 @@ pub struct HypercubeLinearConstructor {
 impl PropagatorConstructor for HypercubeLinearConstructor {
     type PropagatorImpl = HypercubeLinearPropagator;
 
-    fn create(self, context: PropagatorConstructorContext) -> Self::PropagatorImpl {
-        todo!()
+    fn create(self, mut context: PropagatorConstructorContext) -> Self::PropagatorImpl {
+        let HypercubeLinearConstructor {
+            hypercube,
+            linear,
+            constraint_tag,
+        } = self;
+
+        let hypercube_predicates = hypercube
+            .iter_predicates()
+            .map(|predicate| context.register_predicate(predicate))
+            .collect();
+
+        HypercubeLinearPropagator {
+            hypercube,
+            hypercube_predicates,
+            linear,
+        }
     }
 }
 
@@ -43,21 +60,17 @@ impl Propagator for HypercubeLinearPropagator {
         "HypercubeLinear"
     }
 
-    fn notify(
-        &mut self,
-        _context: NotificationContext,
-        _local_id: LocalId,
-        _event: OpaqueDomainEvent,
-    ) -> EnqueueDecision {
-        todo!()
-    }
+    fn propagate_from_scratch(&self, mut context: PropagationContext) -> PropagationStatusCP {
+        let is_hypercube_satisfied = self
+            .hypercube_predicates
+            .iter()
+            .all(|&predicate_id| context.is_predicate_id_satisfied(predicate_id));
 
-    fn propagate(&mut self, context: PropagationContext) -> PropagationStatusCP {
-        todo!()
-    }
+        if is_hypercube_satisfied {
+            todo!()
+        }
 
-    fn propagate_from_scratch(&self, context: PropagationContext) -> PropagationStatusCP {
-        todo!()
+        Ok(())
     }
 }
 
@@ -65,9 +78,9 @@ impl Propagator for HypercubeLinearPropagator {
 mod tests {
     use std::num::NonZero;
 
-    use crate::{predicate, state::State};
-
     use super::*;
+    use crate::predicate;
+    use crate::state::State;
 
     #[test]
     fn conflict_detected() {
@@ -87,7 +100,8 @@ mod tests {
                 (NonZero::new(1).unwrap(), z),
             ],
             5,
-        );
+        )
+        .expect("not trivially false");
 
         let constraint_tag = state.new_constraint_tag();
         let _ = state.add_propagator(HypercubeLinearConstructor {
