0e7329ebf9cd8aad5b4bb2bebba4d5e11383eafb
Maarten Flippo <maartenflippo@outlook.com>
2026-02-27 17:09:04 +1100

    Don't add to supported values of the domain being witnessed

--- pumpkin-crates/core/src/propagation/checkers/bound_consistency_checker.rs
+++ pumpkin-crates/core/src/propagation/checkers/bound_consistency_checker.rs
@@ -1,14 +1,9 @@
 use pumpkin_checking::InferenceChecker;
-use pumpkin_checking::VariableState;
 
-use crate::containers::HashMap;
-use crate::predicate;
 use crate::predicates::Predicate;
 use crate::propagation::Domains;
-use crate::propagation::ReadDomains;
 use crate::propagation::checkers::ConsistencyChecker;
 use crate::propagation::checkers::Scope;
-use crate::propagation::checkers::Witness;
 use crate::propagation::checkers::WitnessGenerator;
 
 #[derive(Clone, Debug)]
@@ -22,79 +17,17 @@ impl<C> BoundConsistencyChecker<C> {
     }
 }
 
-impl<C: InferenceChecker<Predicate>> BoundConsistencyChecker<C> {
-    /// Validate that the witness is a valid solution.
-    fn validate_witness(&self, witness: &Witness) -> bool {
-        let premises = witness
-            .iter()
-            .map(|(domain, value)| predicate![domain == value])
-            .collect::<Vec<_>>();
-
-        let state = VariableState::prepare_for_conflict_check(premises.clone(), None)
-            .expect("the witness is consistent by construction");
-
-        let state_is_conflicting = self.witness_generator.check(state, &premises, None);
-
-        !state_is_conflicting
-    }
-}
-
 impl<C> ConsistencyChecker for BoundConsistencyChecker<C>
 where
     C: WitnessGenerator + InferenceChecker<Predicate> + Clone,
 {
     fn check_consistency(&self, domains: Domains<'_>, scope: &Scope) -> bool {
-        // A propagator is domain consistent if all values for all variables participate in
-        // at least one solution to the constraint.
-
-        let mut supported_values = HashMap::new();
-
-        for (local_id, domain_id) in scope.iter() {
-            let supported_values_in_domain: Vec<_> =
-                std::mem::take(supported_values.entry(domain_id).or_default());
-
-            for value in [
-                domains.lower_bound(&domain_id),
-                domains.upper_bound(&domain_id),
-            ] {
-                if supported_values_in_domain.contains(&value) {
-                    // If the value is already supported, no need to generate a witness
-                    // for it.
-                    continue;
-                }
-
-                let witness = self
-                    .witness_generator
-                    .support(&domains, local_id, value.into());
-
-                assert_eq!(
-                    Some(value),
-                    witness.value_for(domain_id),
-                    "the witness for a variable assignment must contain that variable assignment"
-                );
-
-                assert!(
-                    self.validate_witness(&witness),
-                    "witness should satisfy the constraint"
-                );
-
-                // Add all the assigned variables as supported values as well.
-                for (witness_domain_id, witness_value) in witness.iter() {
-                    assert!(
-                        domains.contains(&witness_domain_id, witness_value),
-                        "witness uses values outside domain"
-                    );
-
-                    let supports = supported_values.entry(witness_domain_id).or_default();
-                    supports.push(witness_value);
-                }
-            }
-
-            let dummy = supported_values
-                .insert(domain_id, supported_values_in_domain)
-                .expect("a element was inserted at the beginning of the loop");
-            assert!(dummy.is_empty(), "no values should be put into the dummy");
-        }
+        super::assert_consistency(
+            domains,
+            scope,
+            &self.witness_generator,
+            super::Consistency::Bounds,
+        );
 
         true
     }
--- pumpkin-crates/core/src/propagation/checkers/mod.rs
+++ pumpkin-crates/core/src/propagation/checkers/mod.rs
@@ -9,12 +9,20 @@ mod witness_generator;
 pub use bound_consistency_checker::*;
 pub use consistency_checker::*;
 pub use domain_consistency_checker::*;
+use pumpkin_checking::InferenceChecker;
+use pumpkin_checking::VariableState;
 pub use scope::*;
 pub use variable::*;
 pub use witness::*;
 pub use witness_generator::*;
 
+use crate::containers::HashMap;
+use crate::predicate;
+use crate::predicates::Predicate;
 use crate::propagation::Domains;
+use crate::propagation::LocalId;
+use crate::propagation::ReadDomains;
+use crate::variables::DomainId;
 
 #[deprecated = "only here to aid refactoring"]
 #[doc(hidden)]
@@ -26,4 +34,111 @@ impl ConsistencyChecker for DefaultChecker {
     fn check_consistency(&self, _: Domains<'_>, _: &Scope) -> bool {
         true
     }
+}
+
+enum Consistency {
+    Bounds,
+    Domain,
+}
+
+fn assert_consistency<PropagationChecker>(
+    domains: Domains<'_>,
+    scope: &Scope,
+    witness_generator: &PropagationChecker,
+    consistency: Consistency,
+) where
+    PropagationChecker: WitnessGenerator + InferenceChecker<Predicate>,
+{
+    let mut supported_values: HashMap<DomainId, Vec<i32>> = HashMap::default();
+
+    for (local_id, domain_id) in scope.iter() {
+        let _ = supported_values.entry(domain_id).or_insert(vec![]);
+
+        match consistency {
+            Consistency::Domain => {
+                // A propagator is domain consistent if all values for all variables participate in
+                // at least one solution to the constraint.
+                for value in domains.iterate_domain(&domain_id) {
+                    support_value(
+                        &domains,
+                        witness_generator,
+                        local_id,
+                        domain_id,
+                        value,
+                        &mut supported_values,
+                    );
+                }
+            }
+            Consistency::Bounds => {
+                for value in [
+                    domains.lower_bound(&domain_id),
+                    domains.upper_bound(&domain_id),
+                ] {
+                    support_value(
+                        &domains,
+                        witness_generator,
+                        local_id,
+                        domain_id,
+                        value,
+                        &mut supported_values,
+                    );
+                }
+            }
+        }
+    }
+}
+
+fn support_value<PropagationChecker>(
+    domains: &Domains<'_>,
+    witness_generator: &PropagationChecker,
+    local_id: LocalId,
+    domain_id: DomainId,
+    value: i32,
+    supported_values: &mut HashMap<DomainId, Vec<i32>>,
+) where
+    PropagationChecker: WitnessGenerator + InferenceChecker<Predicate>,
+{
+    if supported_values[&domain_id].contains(&value) {
+        // If the value is already supported, no need to generate a witness
+        // for it.
+        return;
+    }
+
+    let witness = witness_generator.support(domains, local_id, value.into());
+
+    assert_eq!(
+        Some(value),
+        witness.value_for(domain_id),
+        "the witness for a variable assignment must contain that variable assignment"
+    );
+
+    assert!(
+        validate_witness(witness_generator, &witness),
+        "witness should satisfy the constraint"
+    );
+
+    // Add all the assigned variables as supported values as well.
+    for (witness_domain_id, witness_value) in witness.iter() {
+        assert!(
+            domains.contains(&witness_domain_id, witness_value),
+            "witness uses values outside domain"
+        );
+
+        let supports = supported_values.entry(witness_domain_id).or_default();
+        supports.push(witness_value);
+    }
+}
+
+fn validate_witness(checker: &impl InferenceChecker<Predicate>, witness: &Witness) -> bool {
+    let premises = witness
+        .iter()
+        .map(|(domain, value)| predicate![domain == value])
+        .collect::<Vec<_>>();
+
+    let state = VariableState::prepare_for_conflict_check(premises.clone(), None)
+        .expect("the witness is consistent by construction");
+
+    let state_is_conflicting = checker.check(state, &premises, None);
+
+    !state_is_conflicting
 }
