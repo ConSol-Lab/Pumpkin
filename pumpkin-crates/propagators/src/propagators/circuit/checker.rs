dca751592902770a31593b0b11614999dac28452
Maarten Flippo <maartenflippo@outlook.com>
2026-03-13 12:56:00 +1100

    More bug fixes

--- pumpkin-crates/propagators/src/propagators/circuit/checker.rs
+++ pumpkin-crates/propagators/src/propagators/circuit/checker.rs
@@ -20,21 +20,38 @@ where
         _premises: &[Atomic],
         _consequent: Option<&Atomic>,
     ) -> bool {
-        for successor in self.successors.iter() {
+        // The indexes that are part of the current chain. This is reset for every variable that we
+        // test for membership of a cycle.
+        let mut current_chain = FixedBitSet::with_capacity(self.successors.len());
+
+        // Nodes that we have visited in the search for cycles. Any nodes that we have already
+        // tested for cycles by following chains do not have to be tested for cycles starting at
+        // them.
+        let mut visited = FixedBitSet::with_capacity(self.successors.len());
+
+        for (visiting_idx, successor) in self.successors.iter().enumerate() {
+            if visited.contains(visiting_idx) {
+                continue;
+            }
+
+            visited.insert(visiting_idx);
+
             let Some(next_node) = successor.induced_fixed_value(&state) else {
                 continue;
             };
 
             // circuit is 1-indexed
             let mut next_idx = usize::try_from(next_node).unwrap() - 1;
-
-            let mut visited = FixedBitSet::with_capacity(self.successors.len());
+            current_chain.clear();
 
             loop {
-                if visited.contains(next_idx) && visited.count_ones(..) < self.successors.len() {
-                    return true;
+                if current_chain.contains(next_idx) {
+                    // Here we are in a cycle. That is a conflict if the length of the
+                    // cycle is less than the number of nodes in the graph.
+                    return current_chain.count_ones(..) < self.successors.len();
                 }
 
+                current_chain.insert(next_idx);
                 visited.insert(next_idx);
 
                 let Some(next_node) = self.successors[next_idx].induced_fixed_value(&state) else {
@@ -54,6 +71,7 @@ mod tests {
     use pumpkin_checking::Comparison;
     use pumpkin_checking::TestAtomic;
     use pumpkin_checking::VariableState;
+    use pumpkin_checking::test_atom;
 
     use super::*;
 
@@ -104,5 +122,25 @@ mod tests {
             .expect("conflicting domain");
 
         assert!(checker.check(state, &premises, Some(&consequent)));
+    }
+
+    #[test]
+    fn hamiltonian_path_is_no_conflict() {
+        let premises = [
+            test_atom![x1 == 2],
+            test_atom![x2 == 3],
+            test_atom![x3 == 1],
+        ];
+
+        let consequent = Some(test_atom![x1 != 2]);
+
+        let state =
+            VariableState::prepare_for_conflict_check(premises, consequent).expect("no conflict");
+
+        let checker = CircuitChecker {
+            successors: ["x1", "x2", "x3"].into(),
+        };
+
+        assert!(!checker.check(state, &premises, consequent.as_ref()));
     }
 }
