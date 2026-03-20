38078cd0853ac58464bd3da91b78aebba563901c
Maarten Flippo <maartenflippo@outlook.com>
2026-03-20 13:32:06 +1100

    chore: Clarify documentation for PredicateHeap

--- pumpkin-proof-processor/src/predicate_heap.rs
+++ pumpkin-proof-processor/src/predicate_heap.rs
@@ -4,28 +4,12 @@ use std::collections::BinaryHeap;
 use pumpkin_core::predicates::Predicate;
 use pumpkin_core::state::State;
 
-/// A heap of predicates. The keys are the trail positions of the predicates in the state.
+/// A max-heap of predicates. The keys are based on the trail positions of the predicates in the
+/// state, meaning predicates are popped in reverse trail order. Implied predicates are popped
+/// before the predicate on the trail that implies the predicate.
 #[derive(Clone, Debug, Default)]
 pub(crate) struct PredicateHeap {
     heap: BinaryHeap<PredicateToExplain>,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-struct PredicateToExplain {
-    predicate: Predicate,
-    priority: usize,
-}
-
-impl PartialOrd for PredicateToExplain {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for PredicateToExplain {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.priority.cmp(&other.priority)
-    }
 }
 
 impl PredicateHeap {
@@ -43,6 +27,8 @@ impl PredicateHeap {
     ///
     /// Its priority will be based on its trail position in the given `state`. This heap will
     /// return elements through [`Self::pop`] by reverse-trail order.
+    ///
+    /// If the predicate is not true in the given state, this method panics.
     pub(crate) fn push(&mut self, predicate: Predicate, state: &State) {
         let trail_position = state
             .trail_position(predicate)
@@ -58,5 +44,27 @@ impl PredicateHeap {
             predicate,
             priority,
         });
+    }
+}
+
+/// Used to order the predicates in the [`PredicateHeap`].
+///
+/// The priority is calculated based on the trail position of the predicate and whether the
+/// predicate is on the trail or implied.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct PredicateToExplain {
+    predicate: Predicate,
+    priority: usize,
+}
+
+impl PartialOrd for PredicateToExplain {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for PredicateToExplain {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.priority.cmp(&other.priority)
     }
 }
