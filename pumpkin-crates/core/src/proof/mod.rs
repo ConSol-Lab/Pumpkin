9b9e7ee9c512950c5b0b4644b6ed7072053f50fd
Maarten Flippo <maartenflippo@outlook.com>
2026-03-28 15:53:10 +1100

    fix(pumpkin-conflict-resolvers): Log inference for freshly added falsified clause

--- pumpkin-crates/core/src/proof/mod.rs
+++ pumpkin-crates/core/src/proof/mod.rs
@@ -345,19 +345,19 @@ impl ProofLog {
             Err(error) => {
                 match error {
                     InvalidDeduction::NoConflict(ignored_inferences) => {
+                        eprintln!("Supporting inferences:");
+                        for inference in self.supporting_inferences.iter() {
+                            eprintln!("{:?} -> {:?}", inference.premises, inference.consequent);
+                        }
+
                         if !ignored_inferences.is_empty() {
-                            eprintln!("Using inferences that cannot be applied:");
+                            eprintln!("Ignored inferences:");
                             for ignored_inference in ignored_inferences {
                                 eprintln!(
                                     "{:?} -> {:?}",
                                     ignored_inference.inference.premises,
                                     ignored_inference.inference.consequent
                                 );
-                            }
-
-                            eprintln!("All inferences:");
-                            for inference in self.supporting_inferences.iter() {
-                                eprintln!("{:?} -> {:?}", inference.premises, inference.consequent);
                             }
                         }
                     }
