aa61ca2dd477c9f7e7195e2b8efc5e350bc99532
Maarten Flippo <maartenflippo@outlook.com>
2026-04-11 11:42:34 +1000

    Implement Display for Hypercube

--- pumpkin-crates/core/src/hypercube_linear/hypercube.rs
+++ pumpkin-crates/core/src/hypercube_linear/hypercube.rs
@@ -1,5 +1,7 @@
+use std::fmt::Display;
 use std::hash::Hash;
 
+use itertools::Itertools;
 use pumpkin_checking::IntExt;
 use pumpkin_checking::VariableState;
 
@@ -124,6 +126,12 @@ impl Hypercube {
     /// Predicates are yielded in sorted order.
     pub fn iter_predicates(&self) -> impl Iterator<Item = Predicate> + '_ {
         self.predicates.iter().copied()
+    }
+}
+
+impl Display for Hypercube {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.iter_predicates().format(" & "))
     }
 }
 
