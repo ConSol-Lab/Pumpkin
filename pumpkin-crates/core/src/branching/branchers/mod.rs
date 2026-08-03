76d562c399d2d22e7bc9b5d9d92a354fb2fbf72f
Imko Marijnissen <imko.marijnissen@gmail.com>
2026-08-03 14:17:42 +0200

    chore: remove no-decision brancher

--- pumpkin-crates/core/src/branching/branchers/mod.rs
+++ pumpkin-crates/core/src/branching/branchers/mod.rs
@@ -4,7 +4,6 @@ pub mod alternating;
 pub mod autonomous_search;
 pub mod dynamic_brancher;
 pub mod independent_variable_value_brancher;
-pub mod no_decision;
 pub mod warm_start;
 #[cfg(doc)]
 use super::Brancher;
